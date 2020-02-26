a12953a28ae63a464e6013f9b1e1ab3bc707dcd8
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-02-26 17:58:09 +0700

    [CHORES] Improve code quality to match sonarqube analysis standard

--- src/router.js
+++ src/router.js
@@ -1,21 +1,14 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import VueDemo from '@/components/VueDemo';
-import Messages from '@/components/Messages';
-
-Vue.use(Router);
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: VueDemo,
-    },
-    {
-      path: '/messages',
-      name: 'messages',
-      component: Messages,
-    },
-  ],
-});
+// import Vue from 'vue';
+// import Router from 'vue-router';
+//
+// Vue.use(Router);
+//
+// export default new Router({
+//  routes: [
+//    {
+//      //      path: '',
+//      //      name: '',
+//      //      component: ,
+//    },
+//  ],
+// });
