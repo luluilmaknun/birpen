4107577015dff90a4c0c79926c939f6d3fd49e23
athallahannafis <athallahannafis@gmail.com>
2020-03-01 18:46:04 +0700

    [RED] Fix unexpected indentation

--- tests/unit/Base.spec.js
+++ tests/unit/Base.spec.js
@@ -9,45 +9,45 @@ describe('Tes base navbar', () => {
     expect(wrapper.html()).toContain('Buat Akun');
   });
 
-    it('apakah base navbar ada tombol Login', () => {
-      const button = wrapper.find({ref: 'login'});
-      expect(button.exists()).toBe(true);
-      expect(wrapper.html()).toContain('Login');
-    });
+  it('apakah base navbar ada tombol Login', () => {
+    const button = wrapper.find({ref: 'login'});
+    expect(button.exists()).toBe(true);
+    expect(wrapper.html()).toContain('Login');
+  });
 
-    it('apakah base navbar ada tombol Pengumuman', () => {
-      const button = wrapper.find({ref: 'pengumuman-button'});
-      expect(button.exists()).toBe(true);
-      expect(wrapper.html()).toContain('Pengumuman');
-    });
+  it('apakah base navbar ada tombol Pengumuman', () => {
+    const button = wrapper.find({ref: 'pengumuman-button'});
+    expect(button.exists()).toBe(true);
+    expect(wrapper.html()).toContain('Pengumuman');
+  });
 
-    it('apakah base navbar ada tombol Surat', () => {
-      const button = wrapper.find({ref: 'surat-button'});
-      expect(button.exists()).toBe(true);
-      expect(wrapper.html()).toContain('Surat');
-    });
+  it('apakah base navbar ada tombol Surat', () => {
+    const button = wrapper.find({ref: 'surat-button'});
+    expect(button.exists()).toBe(true);
+    expect(wrapper.html()).toContain('Surat');
+  });
 
-    it('button Surat redirect', () => {
-      const button = wrapper.find({ref: 'surat-button'});
-      window.location.assign = jest.fn();
-      button.trigger('click');
-      expect(window.location.assign).toHaveBeenCalledWith('/surat');
-    });
+  it('button Surat redirect', () => {
+    const button = wrapper.find({ref: 'surat-button'});
+    window.location.assign = jest.fn();
+    button.trigger('click');
+    expect(window.location.assign).toHaveBeenCalledWith('/surat');
+  });
 
-    it('button Pengumuman redirect', () => {
-      const button = wrapper.find({ref: 'pengumuman-button'});
-      window.location.assign = jest.fn();
-      button.trigger('click');
-      expect(window.location.assign).toHaveBeenCalledWith('/pengumuman');
-    });
+  it('button Pengumuman redirect', () => {
+    const button = wrapper.find({ref: 'pengumuman-button'});
+    window.location.assign = jest.fn();
+    button.trigger('click');
+    expect(window.location.assign).toHaveBeenCalledWith('/pengumuman');
+  });
 
 
-    it('button Login redirect', () => {
-      const button = wrapper.find({ref: 'login'});
-      window.location.assign = jest.fn();
-      button.trigger('click');
-      expect(window.location.assign).toHaveBeenCalledWith('/login');
-    });
+  it('button Login redirect', () => {
+    const button = wrapper.find({ref: 'login'});
+    window.location.assign = jest.fn();
+    button.trigger('click');
+    expect(window.location.assign).toHaveBeenCalledWith('/login');
+  });
 
   it('button buat akun redirect', () => {
     const button = wrapper.find({ref: 'buat-akun'});
