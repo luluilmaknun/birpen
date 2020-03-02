fb398d06aca8d9825578cad9aef158be22575f5b
annida.safira <annida.safira@ui.ac.id>
2020-03-02 13:48:30 +0700

    [REFACTOR] views.py login method

--- pengumuman/views.py
+++ pengumuman/views.py
@@ -27,23 +27,22 @@ def pengumuman_placeholder_views(_):
 
 
 @csrf_exempt
-@api_view(["POST","GET"])
+@api_view(["POST", "GET"])
 @permission_classes((AllowAny,))
 def login(request):
     if request.method == "GET":
         response = {}
         return render(request, 'login.html', response)
 
-    elif request.method=="POST":
-        username = request.data.get("username")
-        password = request.data.get("password")
-        if username is None or password is None:
-            return Response({'error': 'Please provide both username and password'},
-                            status=HTTP_400_BAD_REQUEST)
-        user = authenticate(username=username, password=password)
-        if not user:
-            return Response({'error': 'Invalid Credentials'},
-                            status=HTTP_404_NOT_FOUND)
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key, 'role': user.user_type},
-                        status=HTTP_200_OK)
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'},
+                        status=HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key, 'role': user.user_type},
+                    status=HTTP_200_OK)
