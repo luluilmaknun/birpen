9c748fc9455c5e7517ce848945bcb7a84fbd89c4
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-03-02 13:30:21 +0700

    [GREEN] date-picker component test

---
+++ src/components/date-picker.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <date-pick
+      v-model="date"
+      :inputAttributes="{readonly: true}"
+      :displayFormat="'DD-MM-YYYY'">
+    </date-pick>
+  </div>
+</template>
+
+<script>
+import DatePick from 'vue-date-pick';
+import 'vue-date-pick/dist/vueDatePick.css';
+
+export default {
+  components: {DatePick},
+  data: () => ({
+    date: '',
+  }),
+};
+</script>
