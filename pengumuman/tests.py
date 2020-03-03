0ac09c9e332e0e016510665b6ca1fe072ebdf2a9
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-03-03 13:19:29 +0700

    [REFACTOR] pengumuman test

--- pengumuman/tests.py
+++ pengumuman/tests.py
@@ -3,11 +3,11 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from pengumuman.apps import PengumumanConfig
 
+
 class LandingPageConfigTest(TestCase):
     def test_apps(self):
         self.assertEqual(PengumumanConfig.name, 'pengumuman')
         self.assertEqual(apps.get_app_config('pengumuman').name, 'pengumuman')
-
 
 class LandingPageApiTest(TestCase):
     def test_get_pengumuman(self):
