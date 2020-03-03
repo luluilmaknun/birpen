f6780ce4905b52f74522ce25a97c91baf98dfeda
annida.safira <annida.safira@ui.ac.id>
2020-03-03 23:43:14 +0700

    [GREEN] implement get pengumuman default

--- pengumuman/urls.py
+++ pengumuman/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import pengumuman_placeholder_views, login
-
+from .views import pengumuman_placeholder_views, login, get_pengumuman_default
 
 urlpatterns = [
     path('', pengumuman_placeholder_views, name='pengumuman_placeholder_views'),
     path('login', login, name='pengumuman_login'),
+    path('get-pengumuman', get_pengumuman_default, name='get_pengumuman'),
 ]
--- pengumuman/views.py
+++ pengumuman/views.py
@@ -1,15 +1,20 @@
+from datetime import timedelta, date
+
 from django.contrib.auth import authenticate
+from django.core import serializers
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND,
     HTTP_200_OK
 )
+
+from .models import Pengumuman
 
 
 # Create your views here.
@@ -46,3 +51,23 @@ def login(request):
     token, _ = Token.objects.get_or_create(user=user)
     return Response({'token': token.key, 'role': user.user_type},
                     status=HTTP_200_OK)
+
+
+@api_view(["GET"])
+@permission_classes((IsAuthenticated,))
+def get_pengumuman_default(request):
+    curr_date = date.today()
+    tomo_date = curr_date + timedelta(days=1)
+    # if user is admin, return all include soft delete
+    if request.user.user_type == 4:
+        filter_today = Pengumuman.all_objects.filter(tanggal_kelas__date=curr_date)
+        pengumuman_today = serializers.serialize("json", filter_today)
+        filter_tomo = Pengumuman.all_objects.filter(tanggal_kelas__date=tomo_date)
+        pengumuman_tomo = serializers.serialize("json", filter_tomo)
+    else:
+        filter_today = Pengumuman.objects.filter(tanggal_kelas__date=curr_date)
+        pengumuman_today = serializers.serialize("json", filter_today)
+        filter_tomo = Pengumuman.objects.filter(tanggal_kelas__date=tomo_date)
+        pengumuman_tomo = serializers.serialize("json", filter_tomo)
+    return Response({"pengumuman_today": pengumuman_today,
+                     "pengumuman_tomo": pengumuman_tomo}, status=200)
