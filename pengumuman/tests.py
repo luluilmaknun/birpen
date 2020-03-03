706dd30efefdaca732a8379e95a6072451e67413
annida.safira <annida.safira@ui.ac.id>
2020-03-04 03:26:56 +0700

    [RED] add test for filter

--- pengumuman/tests.py
+++ pengumuman/tests.py
@@ -1,16 +1,17 @@
+import json
+
 from django.apps import apps
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 from django.test import TestCase
-
 from django.utils.datastructures import MultiValueDict
 from django.utils.http import urlencode
-
-from django.db import IntegrityError
-
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APIRequestFactory
+from rest_framework.test import force_authenticate
 
 from pengumuman.apps import PengumumanConfig
-from .models import MataKuliah, JenisPengumuman, Ruang, Sesi, StatusPengumuman, Pengumuman
+from .models import MataKuliah, JenisPengumuman, Ruang, Sesi, StatusPengumuman, Pengumuman, User
+from .views import filter_pengumuman
 
 
 class LandingPageConfigTest(TestCase):
@@ -226,8 +227,10 @@ class PengumumanModelTest(TestCase):
 
         Pengumuman.objects.create(tanggal_kelas=tanggal_kelas, nama_pembuat="Jaraka",
                                   nama_mata_kuliah=mata_kuliah, jenis_pengumuman=jenis_pengumuman,
-                                  nama_dosen="Dosen S.kom", nama_asisten="Asistenku", nama_ruang=ruang,
-                                  nama_sesi=sesi, nama_status_pengumuman=status_pengumuman, komentar="")
+                                  nama_dosen="Dosen S.kom",
+                                  nama_asisten="Asistenku", nama_ruang=ruang,
+                                  nama_sesi=sesi,
+                                  nama_status_pengumuman=status_pengumuman, komentar="")
 
         count = Pengumuman.objects.all().count()
         self.assertEqual(count, 1)
@@ -246,8 +249,10 @@ class PengumumanModelTest(TestCase):
 
         Pengumuman.objects.create(tanggal_kelas=tanggal_kelas, nama_pembuat="Jaraka",
                                   nama_mata_kuliah=mata_kuliah, jenis_pengumuman=jenis_pengumuman,
-                                  nama_dosen="Dosen S.kom", nama_asisten="Asistenku", nama_ruang=ruang,
-                                  nama_sesi=sesi, nama_status_pengumuman=status_pengumuman, komentar="")
+                                  nama_dosen="Dosen S.kom",
+                                  nama_asisten="Asistenku", nama_ruang=ruang,
+                                  nama_sesi=sesi,
+                                  nama_status_pengumuman=status_pengumuman, komentar="")
         count = Pengumuman.objects.all().count()
         self.assertEqual(count, 1)
 
@@ -256,3 +261,66 @@ class PengumumanModelTest(TestCase):
         count_with_soft_deleted = Pengumuman.all_objects.all().count()
         self.assertEqual(count_without_soft_deleted, 0)
         self.assertEqual(count_with_soft_deleted, 1)
+
+
+class lihat_pengumuman_api_test(TestCase):
+    def setUp(self):
+        user = get_user_model()
+        user.objects.create_user(username='yusuf.tri',
+                                 name='yusuf tri a.', npm='1701837382',
+                                 password='mahasiswa', user_type=1)
+        user.objects.create_user(username='julia.ningrum',
+                                 name='julia ningrum', npm='1204893059',
+                                 password='admin', user_type=4)
+        tanggal_kelas = "2016-11-16T03:31:18.130822+00:00"
+        mata_kuliah = MataKuliah.objects.create(nama="Alin")
+        jenis_pengumuman = JenisPengumuman.objects.create(nama="Asistensi")
+        ruang = Ruang.objects.create(nama="3111")
+        sesi = Sesi.objects.create(nama="16.00 - 17.40")
+        status_pengumuman = StatusPengumuman.objects.create(nama="Ditunda")
+
+        Pengumuman.objects.create(tanggal_kelas=tanggal_kelas, nama_pembuat="Jaraka",
+                                  nama_mata_kuliah=mata_kuliah, jenis_pengumuman=jenis_pengumuman,
+                                  nama_dosen="Dosen S.kom",
+                                  nama_asisten="Asistenku", nama_ruang=ruang,
+                                  nama_sesi=sesi,
+                                  nama_status_pengumuman=status_pengumuman, komentar="")
+
+        Pengumuman.objects.create(tanggal_kelas=tanggal_kelas, nama_pembuat="Fauzan",
+                                  nama_mata_kuliah=mata_kuliah, jenis_pengumuman=jenis_pengumuman,
+                                  nama_dosen="Dosen S.Kom",
+                                  nama_asisten="Asistennya", nama_ruang=ruang,
+                                  nama_sesi=sesi,
+                                  nama_status_pengumuman=status_pengumuman, komentar="")
+
+        Pengumuman.objects.filter(nama_pembuat="Fauzan").delete()
+
+    def test_request_without_authentication(self):
+        client = APIClient()
+        response = client.get('/api/pengumuman/filter-pengumuman?tanggal=16-11-2016')
+
+        self.assertEqual(response.status_code, 401)
+
+    def test_request_not_admin(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='yusuf.tri')
+        view = filter_pengumuman
+
+        # Make an authenticated request to the view...
+        request = factory.get('/api/pengumuman/filter-pengumuman?tanggal=16-11-2016')
+        force_authenticate(request, user=user)
+        response = view(request)
+        data_date = json.loads(response.data["pengumuman_response"])
+        self.assertEqual(len(data_date), 1)
+
+    def test_request_as_admin(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='julia.ningrum')
+        view = filter_pengumuman
+
+        # Make an authenticated request to the view...
+        request = factory.get('/api/pengumuman/filter-pengumuman?tanggal=16-11-2016')
+        force_authenticate(request, user=user)
+        response = view(request)
+        data_date = json.loads(response.data["pengumuman_response"])
+        self.assertEqual(len(data_date), 2)
