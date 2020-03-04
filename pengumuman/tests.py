7e717b1b77d87ddf8297b69023c8cf1a9c4c858f
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-03-05 00:32:13 +0700

    [RED] add test API form dropdown data

--- pengumuman/tests.py
+++ pengumuman/tests.py
@@ -10,11 +10,14 @@ from django.utils.http import urlencode
 from django.db import IntegrityError
 
 from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import force_authenticate
 from rest_framework.authtoken.models import Token
 
 from pengumuman.apps import PengumumanConfig
 from .models import MataKuliah, JenisPengumuman, Ruang, Sesi, StatusPengumuman, Pengumuman, \
     User
+from .views import dropdown_pengumuman
 
 
 class LandingPageConfigTest(TestCase):
@@ -398,3 +401,38 @@ class PengumumanApiTest(TestCase):
         self.assertEqual(response.data['pengumuman']['nama_ruang'], '3311')
         self.assertEqual(response.data['pengumuman']['nama_sesi'], 'Sesi 4 (17.00 - 19.25)')
         self.assertEqual(response.data['pengumuman']['nama_status_pengumuman'], 'Dibatalkan')
+
+class DropdownApiTest(TestCase):
+    def setUp(self):
+        User.objects.create(username='julia.ningrum', name='julia ningrum',
+                            npm='1204893059', password='admin', user_type=User.ADMIN)
+        User.objects.create(username='yusuf.tri',
+                            name='yusuf tri a.', npm='1701837382',
+                            password='mahasiswa', user_type=User.MAHASISWA)
+
+    def test_request_as_admin(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='julia.ningrum')
+        view = dropdown_pengumuman
+
+        JenisPengumuman.objects.create(nama='Asistensi')
+        MataKuliah.objects.create(nama='Sistem Cerdas')
+        Ruang.objects.create(nama='3111')
+        Sesi.objects.create(nama='Sesi 1 (08.00 - 10.30)')
+        StatusPengumuman.objects.create(nama='Terlambat')
+
+        # Make an authenticated request to the view...
+        request = factory.get('/api/pengumuman/dropdown')
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_data_not_exist(self):
+        factory = APIRequestFactory()
+        user = User.objects.get(username='julia.ningrum')
+        view = dropdown_pengumuman
+
+        request = factory.get('/api/pengumuman/dropdown')
+        force_authenticate(request, user=user)
+        response = view(request)
+        self.assertEqual(response.status_code, 400)
