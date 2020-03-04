bd5af6a9136bf6cef46432e00ec4d1cda581a94c
athallahannafis <athallahannafis@gmail.com>
2020-03-04 16:07:15 +0700

    [REFACTOR] Increase responsivity for mobile version

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,7 +6,8 @@
             </h2>
         </div>
 
-        <div class="nav-elem-container-right">
+        <!-- NAVBAR ON DESKTOP VIEW -->
+        <div class="nav-elem-container-right" id="desktop-nav">
             <router-link class="nav-elem"
             ref="surat-button" :to="{ name: 'surat' }">
             Surat
@@ -24,19 +25,53 @@
             Login
             </router-link>
         </div>
+        
+        <!-- NAVBAR WHEN WIDTH 700px -->
+        <div class="nav-700px">
+            <button class="toggle-dropdown"
+            v-on:click="mobileView = !mobileView">
+            Menu List <img id="dropdown-img" 
+            src="./../assets/white-arrow-png-41944.png"/>
+            </button>
+
+            <div class="nav-elem-container-right" 
+            id="mob-nav" v-show="mobileView">
+                <router-link class="nav-elem"
+                ref="surat-button" :to="{ name: 'surat' }">
+                Surat
+                </router-link>
+                <router-link class="nav-elem"
+                ref="pengumuman-button" :to="{ name: 'pengumuman' }">
+                Pengumuman
+                </router-link>
+                <router-link class="nav-elem"
+                ref="buatAkun-button" :to="{ name: 'buat akun' }">
+                Buat Akun
+                </router-link>
+                <router-link class="nav-elem" id="login-button"
+                ref="login" :to="{ name: 'login' }">
+                Login
+                </router-link>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {};
+  data () {
+    return {
+        mobileView: false,
+    };
   },
-};
+}
 </script>
 
 
 <style>
+.nav-700px {
+    display: none;
+}
 .nav-container {
     padding: 20px 20px;
     display: flex;
@@ -95,6 +130,27 @@ h2 {
 }
 
 @media only screen and (max-width:700px) {
+    #dropdown-img {
+        width: 10px;
+        height: 10px;
+    }
+
+    .nav-700px {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .toggle-dropdown {
+        background: none;
+        border-style: none;
+        color: white;
+        margin-top: 5px;
+    }
+
+    #desktop-nav {
+        display: none;
+    }
     .nav-container {
         display: flex;
         flex-direction: column;
@@ -109,6 +165,7 @@ h2 {
     .nav-elem {
         padding: 10px 10px;
         font-size: 10pt;
+        margin-top: 5px;
         width: fit-content;
     }
 
