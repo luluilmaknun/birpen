5e33c6f18e3c3f6e90decbaa38b8cdb41e8ebcfb
annida.safira <annida.safira@ui.ac.id>
2020-03-04 15:51:03 +0700

    [GREEN] implement modification

--- pengumuman/urls.py
+++ pengumuman/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import pengumuman_placeholder_views, login, get_pengumuman_default
+from .views import pengumuman_placeholder_views, login, edit_pengumuman, get_pengumuman_default
+
 
 urlpatterns = [
     path('', pengumuman_placeholder_views, name='pengumuman_placeholder_views'),
     path('login', login, name='pengumuman_login'),
     path('get-pengumuman', get_pengumuman_default, name='get_pengumuman'),
+    path('<str:key>/edit/', edit_pengumuman, name='edit_pengumuman'),
 ]
--- pengumuman/views.py
+++ pengumuman/views.py
@@ -1,7 +1,7 @@
-from datetime import timedelta, date
+from datetime import datetime, timedelta, date
 
 from django.contrib.auth import authenticate
-from django.core import serializers
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
@@ -11,13 +11,13 @@ from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND,
-    HTTP_200_OK
+    HTTP_200_OK,
+    HTTP_403_FORBIDDEN,
 )
 
-from .models import Pengumuman
-
-
-# Create your views here.
+from .models import User, Pengumuman, MataKuliah, JenisPengumuman, \
+    Ruang, Sesi, StatusPengumuman
+from .serializers import PengumumanSerializer
 
 
 @api_view(["GET"])
@@ -27,8 +27,6 @@ def pengumuman_placeholder_views(_):
     }
 
     return Response({"success": True, "result": result}, status=200)
-
-
 
 
 @csrf_exempt
@@ -42,11 +40,11 @@ def login(request):
     username = request.data.get("username")
     password = request.data.get("password")
     if username is None or password is None:
-        return Response({'error': 'Please provide both username and password'},
+        return Response({'detail': 'Please provide both username and password'},
                         status=HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
     if not user:
-        return Response({'error': 'Invalid Credentials'},
+        return Response({'detail': 'Invalid Credentials'},
                         status=HTTP_404_NOT_FOUND)
     token, _ = Token.objects.get_or_create(user=user)
     return Response({'token': token.key, 'role': user.user_type},
@@ -61,13 +59,55 @@ def get_pengumuman_default(request):
     # if user is admin, return all include soft delete
     if request.user.user_type == 4:
         filter_today = Pengumuman.all_objects.filter(tanggal_kelas__date=curr_date)
-        pengumuman_today = serializers.serialize("json", filter_today)
         filter_tomo = Pengumuman.all_objects.filter(tanggal_kelas__date=tomo_date)
-        pengumuman_tomo = serializers.serialize("json", filter_tomo)
     else:
         filter_today = Pengumuman.objects.filter(tanggal_kelas__date=curr_date)
-        pengumuman_today = serializers.serialize("json", filter_today)
         filter_tomo = Pengumuman.objects.filter(tanggal_kelas__date=tomo_date)
-        pengumuman_tomo = serializers.serialize("json", filter_tomo)
+    pengumuman_today = (PengumumanSerializer(x).data for x in filter_today)
+    pengumuman_tomo = (PengumumanSerializer(x).data for x in filter_tomo)
     return Response({"pengumuman_today": pengumuman_today,
                      "pengumuman_tomo": pengumuman_tomo}, status=200)
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
+def edit_pengumuman(request, key):
+    try:
+        pengumuman = Pengumuman.objects.get(pk=key)
+    except Pengumuman.DoesNotExist:
+        return Response({
+            'detail': 'Pengumuman does not exist.'
+        }, status=HTTP_400_BAD_REQUEST)
+
+    if request.user.user_type != User.ADMIN and pengumuman.pembuat != request.user:
+        return Response({
+            'detail': 'Not enough privileges.'
+        }, status=HTTP_403_FORBIDDEN)
+
+    pengumuman.nama_dosen = request.data.get('nama_dosen')
+    pengumuman.nama_asisten = request.data.get('nama_asisten')
+    pengumuman.komentar = request.data.get('komentar')
+
+    try:
+        pengumuman.tanggal_kelas = datetime.strptime(request.data.get('tanggal_kelas'),
+                                                     '%Y-%m-%d')
+        pengumuman.nama_mata_kuliah = \
+            MataKuliah.objects.get(nama=request.data.get('nama_mata_kuliah'))
+        pengumuman.jenis_pengumuman = \
+            JenisPengumuman.objects.get(nama=request.data.get('jenis_pengumuman'))
+        pengumuman.nama_ruang = Ruang.objects.get(nama=request.data.get('nama_ruang'))
+        pengumuman.nama_sesi = Sesi.objects.get(nama=request.data.get('nama_sesi'))
+        pengumuman.nama_status_pengumuman = \
+            StatusPengumuman.objects.get(nama=request.data.get('nama_status_pengumuman'))
+    except (ObjectDoesNotExist, ValueError, TypeError):
+        return Response({
+            'detail': 'Invalid data.'
+        }, status=HTTP_400_BAD_REQUEST)
+
+    pengumuman.save()
+
+    return Response({
+        "success": True,
+        "pengumuman": PengumumanSerializer(pengumuman).data
+    }, status=HTTP_200_OK)
