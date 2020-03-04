d5a97e11dae0ad99d00421b5287115cec98975aa
athallahannafis <athallahannafis@gmail.com>
2020-03-05 01:27:06 +0700

    [CHORES] Add vue-modal-js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
+import VueModal from 'vue-js-modal';
 
 Vue.config.productionTip = false;
 
 // Vue.use(VueRouter);
-
+Vue.use(VueModal);
 const vue = new Vue({
   router,
   render: (h) => h(App),
