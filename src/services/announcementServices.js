016980b29ef620d1804434cf7593daa1f29448d6
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-03-05 12:08:26 +0700

    [RED] Add get and edit services in announcement services

---
+++ src/services/announcementServices.js
@@ -0,0 +1,10 @@
+import api from '@/services/api';
+
+export default {
+  getAnnouncement(key) {
+    return api.get('pengumuman/'+key);
+  },
+  editAnnouncement(key, request) {
+    return api.post('pengumuman/'+key+'/edit/', request);
+  },
+};
