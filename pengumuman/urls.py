4707922bb31c36bc567d21f709162195e93c5456
athallahannafis <athallahannafis@gmail.com>
2020-03-05 09:23:01 +0700

    [RED] Add url for create pengumuman API

--- pengumuman/urls.py
+++ pengumuman/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import pengumuman_placeholder_views, login, edit_pengumuman
+from .views import pengumuman_placeholder_views, login, edit_pengumuman, create_pengumuman
 
 
 urlpatterns = [
     path('', pengumuman_placeholder_views, name='pengumuman_placeholder_views'),
     path('login', login, name='pengumuman_login'),
     path('<str:key>/edit/', edit_pengumuman, name='edit_pengumuman'),
+    path('create/', create_pengumuman, name='create_pengumuman'),
 ]
