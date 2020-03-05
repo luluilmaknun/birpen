2a93a33bb0c31d62d5e64e113fa7248e584e5715
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-03-05 12:08:59 +0700

    [RED] Implement base code for edit announcement
    WIP : Need Create announcement, will rebase

---
+++ src/views/EditAnnouncement.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <CreateAnnouncement :data_edit="data_pengumuman" edit="true" />
+  </div>
+</template>
+
+<script>
+import CreateAnnouncement from '@/views/CreateAnnouncement';
+import announcementApi from '@/services/announcementServices';
+
+export default {
+  name: 'EditAnnouncement',
+  component: CreateAnnouncement,
+  data: function() {
+    announcementApi.getAnnouncement().then((d) => {
+      return d.data;
+    });
+  },
+};
+</script>
