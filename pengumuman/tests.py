23452cdfe2eab221b131e00007b07755f00cdee3
athallahannafis <athallahannafis@gmail.com>
2020-03-05 09:22:15 +0700

    [REFACTOR] Increase quality test

--- pengumuman/tests.py
+++ pengumuman/tests.py
@@ -407,15 +407,23 @@ class CreatePengumumanApiTest(TestCase):
                                      npm='1701837382', password='mahasiswa',
                                      user_type=User.MAHASISWA)
         self.token_1 = Token.objects.get_or_create(user=user_1)[0].key
+
+        tanggal_kelas = "2016-11-16T22:31:18.130822+00:00"
+        mata_kuliah = MataKuliah.objects.create(nama="Aljabar Linier")
+        jenis_pengumuman = JenisPengumuman.objects.create(nama="Asistensi")
+        ruang = Ruang.objects.create(nama="2311")
+        sesi = Sesi.objects.create(nama="Sesi 4 (17.00 - 19.30)")
+        status_pengumuman = StatusPengumuman.objects.create(nama="Terlambat")
+
         self.valid_data = {
-            # 'pembuat': user_1,
-            'tanggal_kelas': '2016-11-12',
-            'nama_mata_kuliah': 'DDP',
-            'jenis_pengumuman': 'Perkuliahan',
-            'nama_dosen': 'Dosen Baru',
-            'nama_ruang': '3311',
-            'nama_sesi': 'Sesi 4 (17.00 - 19.25)',
-            'nama_status_pengumuman': 'Dibatalkan',
+            'tanggal_kelas': '2016-11-16',
+            'nama_mata_kuliah': 'Aljabar Linier',
+            'jenis_pengumuman': 'Asistensi',
+            'nama_dosen': 'Dosen Nafis',
+            'nama_asisten': 'Nida',
+            'nama_ruang': '2311',
+            'nama_sesi': 'Sesi 4 (17.00 - 19.30)',
+            'nama_status_pengumuman': 'Terlambat',
             'komentar': 'Saya kesiangan'
         }
         self.invalid_data = {
@@ -428,12 +436,12 @@ class CreatePengumumanApiTest(TestCase):
                                     data=urlencode(MultiValueDict(self.invalid_data)),
                                     content_type='application/x-www-form-urlencoded')
         self.assertEqual(response.status_code, 400)
-    
+
     def test_can_create(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token_1)
-        self.client.post("user") = user_1
+
         response = self.client.post('/api/pengumuman/create/',
                                     data=urlencode(MultiValueDict(self.valid_data)),
                                     content_type='application/x-www-form-urlencoded')
-        print(self.valid_data)
+        print(response)
         self.assertEqual(response.status_code, 200)
