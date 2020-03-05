d80f46417c42e2a8783accf7be8462c190c00a07
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-03-05 12:35:30 +0700

    [CHORES] Install package on start script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Starting birpen ..."
-env/bin/python3 manage.py makemigrations
+pip3 install -r requirements.txt
+yarn install
+yarn build
 env/bin/python3 manage.py migrate
 sudo env/bin/python3 manage.py runserver 0:80 >out 2>&1 &
