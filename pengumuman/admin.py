2059f89a0ccf3c3aa130609bc9f7c20a084ad55c
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-03-08 16:17:49 +0700

    [GREEN] Fix clash on registering sso_ui.models.Profile to admin site

--- pengumuman/admin.py
+++ pengumuman/admin.py
@@ -1,15 +1,10 @@
 from django.apps import apps
 from django.contrib import admin
 from django.contrib.admin.sites import AlreadyRegistered
-
 from .models import User
+from .apps import PengumumanConfig
 
-models = apps.get_models()
-admin.site.register(User)
+app = apps.get_app_config('pengumuman')
 
-
-for model in models:
-    try:
-        admin.site.register(model)
-    except AlreadyRegistered:
-        pass
+for model_name, model in app.models.items():
+    admin.site.register(model)
