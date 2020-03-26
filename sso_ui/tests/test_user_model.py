fabf9110491340ec0dc8a6847c6f27ce2ae2a5ed
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-03-26 19:01:59 +0700

    [RED] Create tests for checking user is mahasiswa and is dosen

--- sso_ui/tests/test_user_model.py
+++ sso_ui/tests/test_user_model.py
@@ -18,6 +18,21 @@ class UserModelTest(TestCase):
                                                       password='asdos')
         AsistenDosen.objects.create(username=self.user_asisten_dosen.username)
 
+        self.user_without_profile = User.objects.create(username='lulu.ilmaknun',
+                                                        password='lulu')
+        self.user_without_profile.profile.delete()
+        self.user_without_profile.refresh_from_db()
+
+        self.user_mahasiswa = User.objects.create(username='annida.safira',
+                                                  password='nida')
+        self.user_mahasiswa.profile.role = 'mahasiswa'
+        self.user_mahasiswa.profile.save()
+
+        self.user_dosen = User.objects.create(username='ahmad.fauzan',
+                                              password='fauzan')
+        self.user_dosen.profile.role = 'staff'
+        self.user_dosen.profile.save()
+
     def test_is_admin_return_true_for_admin(self):
         self.assertTrue(self.user_admin.is_admin())
 
@@ -29,3 +44,21 @@ class UserModelTest(TestCase):
 
     def test_is_asdos_return_false_for_non_asisten_dosen(self):
         self.assertFalse(self.user.is_asdos())
+
+    def test_is_mahasiswa_return_true_for_mahasiswa(self):
+        self.assertTrue(self.user_mahasiswa.is_mahasiswa())
+
+    def test_is_mahasiswa_return_false_for_non_mahasiswa(self):
+        self.assertFalse(self.user.is_mahasiswa())
+
+    def test_is_mahasiswa_return_false_if_no_profile(self):
+        self.assertFalse(self.user_without_profile.is_mahasiswa())
+
+    def test_is_dosen_return_true_for_dosen(self):
+        self.assertTrue(self.user_dosen.is_dosen())
+
+    def test_is_dosen_return_false_for_non_dosen(self):
+        self.assertFalse(self.user.is_dosen())
+
+    def test_is_dosen_return_false_if_no_profile(self):
+        self.assertFalse(self.user_without_profile.is_dosen())
