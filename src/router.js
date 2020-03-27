c3423f56209f2e3cc7ddb7cc4e5245fff552be1f
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-03-27 08:15:30 +0700

    [CHORES] solve conflict in router.js

--- src/router.js
+++ src/router.js
@@ -1,24 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-<<<<<<< HEAD
-import Pengumuman from './components/Pengumuman.vue';
-=======
 import Home from '@/Home.vue';
 import Delete from '@/components/delete.vue';
 import CreateAnnouncement from '@/views/CreateAnnouncement.vue';
 import EditAnnouncement from '@/views/EditAnnouncement.vue';
->>>>>>> PBI-1-CRUD_Pengumuman
 
 Vue.use(Router);
 
 export default new Router({
-<<<<<<< HEAD
-  routes: [
-    {
-      path: '/pengumuman',
-      name: 'pengumuman',
-      component: Pengumuman,
-=======
   mode: 'history',
   routes: [
     {
@@ -46,7 +35,6 @@ export default new Router({
       name: 'edit-pengumuman',
       component: EditAnnouncement,
       props: true,
->>>>>>> PBI-1-CRUD_Pengumuman
     },
   ],
 });
