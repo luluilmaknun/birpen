7ad9d1e790243a71e5d1137299120cc7922e2dca
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-04-03 23:46:27 +0700

    [GREEN] Create a temp file delete asdos implementation
    The real implementation must wait for another issue

--- asdos/urls.py
+++ asdos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import asdos_placeholder_views
+from .views import asdos_placeholder_views, delete_asdos
 
 
 urlpatterns = [
     path('', asdos_placeholder_views, name='asdos_placeholder_views'),
+    path('<str:key>/delete/', delete_asdos, name='delete_asdos'),
 ]
--- asdos/views.py
+++ asdos/views.py
@@ -1,8 +1,14 @@
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_200_OK,
+)
 
+ASDOS_NOT_FOUND_MESSAGE = 'Asdos does not exist.'
 
-# Create your views here.
 
 @api_view(["GET"])
 def asdos_placeholder_views(_):
@@ -11,3 +17,21 @@ def asdos_placeholder_views(_):
     }
 
     return Response({"success": True, "result": result}, status=200)
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
+def delete_asdos(request, key):
+    try:
+        asdos = Asdos.objects.get(pk=key)
+    except Asdos.DoesNotExist:
+        return Response({
+            'detail': ASDOS_NOT_FOUND_MESSAGE
+        }, status=HTTP_400_BAD_REQUEST)
+
+    asdos.delete()
+
+    return Response({
+        "success": True,
+    }, status=HTTP_200_OK)
