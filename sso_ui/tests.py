83afe9841eb3b3d5f18eaac7c2a27612346305c4
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-05 22:05:58 +0700

    [RED] Create tests for saving user information to localStorage after login

--- sso_ui/tests.py
+++ sso_ui/tests.py
@@ -97,13 +97,15 @@ class SSOUITest(TestCase):
                                         name='yusuf tri a.', npm='1701837382',
                                         password='mahasiswa', user_type=User.MAHASISWA)
         self.client.force_login(user)
-        response = self.client.get(reverse('sso_ui:create-token'))
+        response = self.client.get(reverse('sso_ui:save_user_info'))
 
         self.assertEqual(response.status_code, 200)
         self.assertNotEqual(response.context['token'], '')
+        self.assertEqual(response.context['username'], 'yusuf.tri')
 
     def test_fail_create_token_user_not_authenticated(self):
-        response = self.client.get(reverse('sso_ui:create-token'))
+        response = self.client.get(reverse('sso_ui:save_user_info'))
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['token'], '')
+        self.assertEqual(response.context['username'], '')
