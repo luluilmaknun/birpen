ab04fee7f88eda844ff510440404d5a641f4119b
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-04-06 21:45:11 +0700

    [REFACTOR] Update existed files to adapt with new authorization system

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const token = sessionStorage.token;
+const token = localStorage.getItem('token');
 
 export default axios.create({
   baseURL: '/api',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Token ${token}`,
+    'Authorization': `Bearer ${token}`,
     'X-CSRFToken': Cookies.get('csrftoken'),
   },
 });
