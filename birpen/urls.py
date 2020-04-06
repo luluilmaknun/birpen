ebcac3396ea34d5702ad69a648f89fe45a17d737
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-07 06:30:44 +0700

    [CHORES] Fix missing trailing slash on regex url for linking django url with vue route

--- birpen/urls.py
+++ birpen/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
 
     path('sso/', include('sso_ui.urls')),
 
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^.*/$', TemplateView.as_view(template_name="index.html")),
 
 ]
