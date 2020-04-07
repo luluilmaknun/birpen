4c6ca3fe20fdec114f92cfc1aa2e04acae3ecdcb
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-04-07 14:00:13 +0700

    [CHORES] permissions.py

--- asdos/permissions.py
+++ asdos/permissions.py
@@ -1,7 +1,10 @@
 from rest_framework import permissions
 
-class IsPrivelegesToAccessAsdos(permissions.BasePermission):
 
+class IsPrivelegesToAccessAsdos(permissions.BasePermission):
+    """
+    Permission class to access asdos
+    """
     def has_permission(self, request, view):
         return request.user.is_admin() or \
-        request.user.is_dosen()
+               request.user.is_dosen()
