9cc2d12c081ff6a957ad742537eb012f63614321
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-04-07 16:00:47 +0700

    [REFACTOR] Refactor for clean code style

--- asdos/views.py
+++ asdos/views.py
@@ -2,8 +2,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
-# Create your views here.
-
 @api_view(["GET"])
 def asdos_placeholder_views(_):
     result = {
