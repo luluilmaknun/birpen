d7375e04e51a591b72bebec602bd72a20bb03c37
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-04-07 15:47:07 +0700

    [GREEN] Improve coverage in asdos app

---
+++ asdos/tests/test_landing_page.py
@@ -0,0 +1,20 @@
+from django.apps import apps
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+from asdos.apps import AsdosConfig
+
+class LandingPageConfigTest(TestCase):
+    def test_apps(self):
+        self.assertEqual(AsdosConfig.name, 'asdos')
+        self.assertEqual(apps.get_app_config('asdos').name, 'asdos')
+
+
+class LandingPageApiTest(TestCase):
+    def test_get_page(self):
+        client = APIClient()
+        response = client.get('/api/asdos/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['success'], True)
+        self.assertEqual(response.data['result']['message'], "asdos placeholder message")
