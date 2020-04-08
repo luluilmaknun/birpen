18fb4e5f9c1a37610d128ee4c14187bf5488f67c
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-04-08 16:07:31 +0700

    test 52% coverage

--- tests/unit/Pengumuman.spec.js
+++ tests/unit/Pengumuman.spec.js
@@ -40,34 +40,88 @@ describe('test tabel', () => {
   });
 
   it('Tes tabel today', () => {
-    expect(wrapper.contains('#table-today')).toBe(true);
+    expect(vm.today.length).toBe(0);
+    // VUE GA BISA NGETES V-IF BIND ELEMENT JADI YANG DITES
+    // ISI KONDISI DARI VARIABLE CONDITIONNYA AJA
   });
 
   it('Tes table tomorrow', () => {
-    expect(wrapper.contains('#table-tomorrow')).toBe(true);
+    expect(vm.today.length).toBe(0);
+    // VUE GA BISA NGETES V-IF BIND ELEMENT JADI YANG DITES
+    // ISI KONDISI DARI VARIABLE CONDITIONNYA AJA
+  });
+});
+
+describe('test tabel exists all', () => {
+  let wrapper; let vm;
+
+  const pengumumanToday = [
+    {
+      'pk': 0,
+      'deleted': null,
+      'created_at': '2020-04-08T12:31:04.026691+07:00',
+      'modified_at': '2020-04-08T12:31:04.026691+07:00',
+      'tanggal_kelas': '2020-04-30',
+      'pembuat': 'dosen',
+      'nama_mata_kuliah': 'Analisis Numerik',
+      'jenis_pengumuman': 'Perkuliahan',
+      'nama_dosen': 'Lulu Ilmaknun S.Kom',
+      'nama_asisten': 'Yusuf Tri Ardho',
+      'nama_ruang': '2211',
+      'nama_sesi': 'Sesi 2 (11.00 - 13.30)',
+      'nama_status_pengumuman': 'Terlambat',
+      'komentar': 'Saya ada urusan mendadak',
+    },
+  ];
+
+  const pengumumanTomo = [
+    {
+      'pk': 0,
+      'deleted': null,
+      'created_at': '2020-04-08T12:31:04.026691+07:00',
+      'modified_at': '2020-04-08T12:31:04.026691+07:00',
+      'tanggal_kelas': '2020-04-30',
+      'pembuat': 'dosen',
+      'nama_mata_kuliah': 'Analisis Numerik',
+      'jenis_pengumuman': 'Perkuliahan',
+      'nama_dosen': 'Lulu Ilmaknun S.Kom',
+      'nama_asisten': 'Yusuf Tri Ardho',
+      'nama_ruang': '2211',
+      'nama_sesi': 'Sesi 2 (11.00 - 13.30)',
+      'nama_status_pengumuman': 'Terlambat',
+      'komentar': 'Saya ada urusan mendadak',
+    },
+  ];
+
+  beforeEach(() => {
+    wrapper = shallowMount(Pengumuman);
+    vm = wrapper.vm;
+    announcementApi.getAnnouncementDefault = jest.fn(
+        (d) => Promise.resolve({
+          status: 200,
+          data: {
+            pengumuman_today: pengumumanToday,
+            pengumuman_tomo: pengumumanTomo,
+          },
+        }));
+    vm.fetchData();
   });
 
-  it('tes detail button', () => {
-    getAnnouncementDefaultApi.fetch= jest.fn(() => Promise.resolve({
-      status: 200,
-      data: {
-        pengumuman_tomo: {
-          'pk': 0,
-          'created_at': '2020-03-01',
-          'modified_at': '2020-03-01',
-          'tanggal_kelas': '2020-03-02',
-          'pembuat': 'dosen',
-          'nama_mata_kuliah': 'Analisis Numerik',
-          'jenis_pengumuman': 'Perkuliahan',
-          'nama_dosen': 'Lulu Ilmaknun S.Kom',
-          'nama_asisten': 'Yusuf Tri Ardho',
-          'nama_ruang': '2211',
-          'nama_sesi': 'Sesi 2 (11.00 - 13.30)',
-          'nama_status_pengumuman': 'Terlambat',
-          'komentar': 'Saya ada urusan mendadak',
-        },
-      },
-    }));
-    expect(wrapper.contains('.detail-button')).toBe(true);
+  it('tes elemen table', () => {
+    expect(wrapper.find('#nama_dosen').text())
+        .toBe('Lulu Ilmaknun S.Kom');
+    // LANJUTIN GAN
+  });
+
+  it('Tes tabel today', () => {
+    expect(vm.today.length).toBe(1);
+    // VUE GA BISA NGETES V-IF BIND ELEMENT JADI YANG DITES
+    // ISI KONDISI DARI VARIABLE CONDITIONNYA AJA
+  });
+
+  it('Tes table tomorrow', () => {
+    expect(vm.today.length).toBe(1);
+    // VUE GA BISA NGETES V-IF BIND ELEMENT JADI YANG DITES
+    // ISI KONDISI DARI VARIABLE CONDITIONNYA AJA
   });
 });
