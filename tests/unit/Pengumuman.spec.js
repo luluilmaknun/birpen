d8459557bf55191ef8da8ca695bdcf463ab3ce48
athallahannafis <athallahannafis@gmail.com>
2020-04-08 11:58:25 +0700

    [RED] Add some test

--- tests/unit/Pengumuman.spec.js
+++ tests/unit/Pengumuman.spec.js
@@ -12,6 +12,7 @@ describe('test tabel', () => {
     },
   });
 
+
   it('Tes tabel today', () => {
     expect(wrapper.contains('#table-today')).toBe(true);
   });
@@ -20,9 +21,27 @@ describe('test tabel', () => {
     expect(wrapper.contains('#table-tomorrow')).toBe(true);
   });
 
-  it('Tes get api', () => {
-    getAnnouncementDefaultApi.Pengumuman = jest.fn(() => Promise.resolve({
+  it('tes detail button', () => {
+    getAnnouncementDefaultApi.fetch= jest.fn(() => Promise.resolve({
       status: 200,
+      data: {
+        pengumuman_tomo: {
+          'pk': 0,
+          'created_at': '2020-03-01',
+          'modified_at': '2020-03-01',
+          'tanggal_kelas': '2020-03-02',
+          'pembuat': 'dosen',
+          'nama_mata_kuliah': 'Analisis Numerik',
+          'jenis_pengumuman': 'Perkuliahan',
+          'nama_dosen': 'Lulu Ilmaknun S.Kom',
+          'nama_asisten': 'Yusuf Tri Ardho',
+          'nama_ruang': '2211',
+          'nama_sesi': 'Sesi 2 (11.00 - 13.30)',
+          'nama_status_pengumuman': 'Terlambat',
+          'komentar': 'Saya ada urusan mendadak',
+        },
+      },
     }));
+    expect(wrapper.contains('.detail-button')).toBe(true);
   });
 });
