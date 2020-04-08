426b5d0ee12f936eec6604f68d9e6f5bc990c16c
athallahannafis <athallahannafis@gmail.com>
2020-04-08 12:44:24 +0700

    [REFACTOR] Fix import error in test

--- tests/unit/Pengumuman.spec.js
+++ tests/unit/Pengumuman.spec.js
@@ -1,5 +1,5 @@
 import {shallowMount} from '@vue/test-utils';
-import Pengumuman from '@/components/Pengumuman';
+import Pengumuman from '@/views/Pengumuman';
 
 import getAnnouncementDefaultApi from '@/services/announcementServices';
 
