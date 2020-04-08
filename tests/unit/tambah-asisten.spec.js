0aa3894ca2ba4e5d012ed481e138a9ec7b88e242
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-04-08 15:59:59 +0700

    [RED] create tambah asisten test

--- tests/unit/tambah-asisten.spec.js
+++ tests/unit/tambah-asisten.spec.js
@@ -1,18 +1,70 @@
 import {shallowMount} from '@vue/test-utils';
 import TambahAsisten from '@/components/tambah-asisten.vue';
+import asistenApi from '@/services/asistenServices';
 
 describe('Test tambah asisten component', () => {
   const wrapper = shallowMount(TambahAsisten);
 
-  it('tes tombol tambah', () => {
+  it('tes tombol tambah asisten', () => {
     expect(wrapper.find('#tambah').exists()).toBe(true);
     const detailButton = wrapper.find('#tambah');
     detailButton.trigger('click');
   });
 
-  it('tes tombol tambah asisten', () => {
+  it('tes tambah asisten', () => {
     expect(wrapper.find('#tambah_asisten').exists()).toBe(true);
     const detailButton = wrapper.find('#tambah_asisten');
     detailButton.trigger('click');
+  });
+});
+
+describe('tes tambah asisten', () => {
+  let wrapper; let vm;
+
+  it('success add asisten', () => {
+    wrapper = shallowMount(TambahAsisten, {
+      data() {
+        return {
+          'sso_username': 'yusuf.tri71',
+          'error_message': '',
+        };
+      },
+    });
+
+    asistenApi.createAsisten = jest.fn(() => Promise.resolve({
+      status: 200,
+      data: {
+        detail: 'Valid data.',
+        success: 'True',
+      },
+    }));
+
+    vm = wrapper.vm;
+    vm.click_tambah();
+    expect(vm.sso_username).toEqual('yusuf.tri71');
+  });
+
+  it('fail add asisten', () => {
+    wrapper = shallowMount(TambahAsisten, {
+      data() {
+        return {
+          'sso_username': '',
+          'error_message': 'Invalid username.',
+        };
+      },
+    });
+
+    asistenApi.createAsisten = jest.fn(() => Promise.resolve({
+      status: 400,
+      data: {
+        detail: 'Invalid username.',
+        success: 'False',
+      },
+    }));
+
+    vm = wrapper.vm;
+    vm.click_tambah();
+    expect(vm.sso_username).toEqual('');
+    expect(vm.error_message).toEqual('Invalid username.');
   });
 });
