f020e212a77294779a91dfd3dd04c93db144f62a
athallahannafis <athallahannafis@gmail.com>
2020-04-08 07:05:18 +0700

    [CHORES] Add test for pengumuman component

--- tests/unit/Pengumuman.spec.js
+++ tests/unit/Pengumuman.spec.js
@@ -1,12 +1,28 @@
 import {shallowMount} from '@vue/test-utils';
-import Pengumuman from '@/components/Pengumuman.vue';
+import Pengumuman from '@/components/Pengumuman';
 
-describe('Tes Tabel', () => {
-  const wrapper = shallowMount(Pengumuman);
+import getAnnouncementDefaultApi from '@/services/pengumumanDataService';
 
-  it('apakah tombol detail mengeluarkan modal', () => {
-    expect(wrapper.find('.detail-button').exists()).toBe(true);
-    const detailButton = wrapper.find('.detail-button');
-    detailButton.trigger('click');
+describe('test tabel', () => {
+  const wrapper = shallowMount(Pengumuman, {
+    'mocks': {
+      $session: {
+        get: jest.fn().mockReturnValueOnce('admin').mockReturnValueOnce(4),
+      },
+    },
+  });
+
+  it('Tes tabel today', () => {
+    expect(wrapper.contains('#table-today')).toBe(true);
+  });
+
+  it('Tes table tomorrow', () => {
+    expect(wrapper.contains('#table-tomorrow')).toBe(true);
+  });
+
+  it('Tes get api', () => {
+    getAnnouncementDefaultApi.Pengumuman = jest.fn(() => Promise.resolve({
+      status: 200,
+    }));
   });
 });
