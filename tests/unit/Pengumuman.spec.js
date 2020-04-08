1608284e60bf874d7d77c991b75867a40b44f9fa
athallahannafis <athallahannafis@gmail.com>
2020-04-08 14:14:35 +0700

    [RED] Minor changes to pass the test

--- tests/unit/Pengumuman.spec.js
+++ tests/unit/Pengumuman.spec.js
@@ -4,14 +4,40 @@ import Pengumuman from '@/views/Pengumuman';
 import getAnnouncementDefaultApi from '@/services/announcementServices';
 
 describe('test tabel', () => {
-  const wrapper = shallowMount(Pengumuman, {
-    'mocks': {
-      $session: {
-        get: jest.fn().mockReturnValueOnce('admin').mockReturnValueOnce(4),
-      },
-    },
+  let wrapper; let vm;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Pengumuman);
+    vm = wrapper.vm;
+    announcementApi.getAnnouncementDefault = jest.fn(
+        () => Promise.resolve({
+          status: 200,
+          data: {
+            pengumuman_today: {
+              'pk': 0,
+              'deleted': null,
+              'created_at': '2020-04-08T12:31:04.026691+07:00',
+              'modified_at': '2020-04-08T12:31:04.026691+07:00',
+              'tanggal_kelas': '2020-04-0',
+              'pembuat': 'dosen',
+              'nama_mata_kuliah': 'Analisis Numerik',
+              'jenis_pengumuman': 'Perkuliahan',
+              'nama_dosen': 'Lulu Ilmaknun S.Kom',
+              'nama_asisten': 'Yusuf Tri Ardho',
+              'nama_ruang': '2211',
+              'nama_sesi': 'Sesi 2 (11.00 - 13.30)',
+              'nama_status_pengumuman': 'Terlambat',
+              'komentar': 'Saya ada urusan mendadak',
+            },
+          },
+        }));
+    vm.fetchData();
   });
 
+  it('tes elemen table', () => {
+    expect(wrapper.find('#nama_dosen').element.value)
+        .toBe('Lulu Ilmaknun S.Kom');
+  });
 
   it('Tes tabel today', () => {
     expect(wrapper.contains('#table-today')).toBe(true);
