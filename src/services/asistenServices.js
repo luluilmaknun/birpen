906e26981e0dd90ef658ba38f3766e6d83471561
athallahannafis <athallahannafis@gmail.com>
2020-04-09 11:18:16 +0700

    [CHORES] Add delete asdos api

--- src/services/asistenServices.js
+++ src/services/asistenServices.js
@@ -4,4 +4,7 @@ export default {
   createAsisten(request) {
     return api.post('asdos/create-asisten/', request);
   },
+  deleteAsisten(request) {
+    return api.post('asdos/delete/', request);
+  },
 };
