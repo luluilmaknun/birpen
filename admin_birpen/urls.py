2d8fd5ab30b224c0d99ce6eba3013e2198383a73
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-13 13:16:36 +0700

    [GREEN] Implement delete admin API

--- admin_birpen/urls.py
+++ admin_birpen/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import admin_placeholder_views
+from .views import admin_placeholder_views, delete_admin
 
 urlpatterns = [
     path('', admin_placeholder_views, name='admin_placeholder_views'),
+    path('<str:username>/delete/', delete_admin, name='delete_admin'),
 ]
--- admin_birpen/views.py
+++ admin_birpen/views.py
@@ -1,8 +1,16 @@
-from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
     HTTP_200_OK,
 )
+
+from .models import Admin
+from .permissions import IsPrivilegedToAccessAdmin
+
+ADMIN_NOT_FOUND_MESSAGE = "Admin does not exist."
 
 
 @api_view(["GET"])
@@ -14,4 +22,21 @@ def admin_placeholder_views(_):
     return Response({
         "success": True,
         "result": result
+    }, status=HTTP_200_OK)
+
+@csrf_exempt
+@api_view(["DELETE"])
+@permission_classes((IsAuthenticated, IsPrivilegedToAccessAdmin,))
+def delete_admin(_, username):
+    try:
+        admin = Admin.objects.get(username=username)
+    except Admin.DoesNotExist:
+        return Response({
+            "detail": ADMIN_NOT_FOUND_MESSAGE
+        }, status=HTTP_400_BAD_REQUEST)
+
+    admin.delete()
+
+    return Response({
+        "success": True,
     }, status=HTTP_200_OK)
