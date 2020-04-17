d1641a8aee6551c2d8898f72e85a88a05566cbcd
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-04-17 23:25:29 +0700

    [REFACTOR] Beautify main menu surat

--- src/views/MainMenuSurat.vue
+++ src/views/MainMenuSurat.vue
@@ -15,7 +15,8 @@
         <div id="button_dokumen_sidang_akhir"
           class="menu-item" @click="goToPage('/surat/sidang/')">
           <div class="menu-image-container">
-            <img class="home-surat-menu" />
+            <img src="@/assets/images/sidang.png" class="menu-icon"
+              alt="Klik untuk menuju layanan sidang akhir" />
           </div>
           <h3>Dokumen Kelengkapan Sidang Karya Akhir</h3>
         </div>
@@ -23,7 +24,8 @@
         <div id="button_pemesanan_dokumen"
           class="menu-item" @click="goToPage('/surat/pemesanan/')">
           <div class="menu-image-container">
-            <img class="home-surat-menu" />
+            <img src="@/assets/images/pemesanan.png" class="menu-icon"
+              alt="Klik untuk menuju layanan pemesanan dokumen" />
           </div>
           <h3>Pemesanan Dokumen Akademik</h3>
         </div>
@@ -31,7 +33,8 @@
         <div id="button_tracking_pemesanan"
           class="menu-item" @click="goToPage('/surat/tracking/')">
           <div class="menu-image-container">
-            <img class="home-surat-menu" />
+            <img src="@/assets/images/tracking.png" class="menu-icon"
+              alt="Klik untuk menuju layanan tracking pemesanan" />
           </div>
           <h3>Trace & Tracking Pemesanan Dokumen Akademik</h3>
         </div>
@@ -61,13 +64,13 @@ body, html {
   padding: 0;
   box-sizing: border-box;
 }
-.home_surat {
+.home-surat {
   height: 100%;
   padding: 2%;
   height: 100%;
   box-sizing: border-box;
 }
-h2.title_home {
+h2.title-home {
   font-family: 'Montserrat', sans-serif;
   font-style: normal;
   font-weight: bold;
@@ -77,8 +80,46 @@ h2.title_home {
   margin-top: 10px;
   text-align: center;
 }
-@media only screen and (max-width: 1600px) {
+.menu-container {
+  background: #2D3033;
+  color: white;
+  margin: 50px;
+  padding: 40px;
+  border-radius: 18px;
+  max-width: 400px;
+}
+.menu-item-list {
+  display: flex;
+  flex-direction: column;
+}
+.menu-item {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  transition: 0s color;
+}
+.menu-item:hover {
+  background: white;
+  color: black;
+  border-radius: 18px;
+}
+img.menu-icon {
+  width: 87px;
+  margin: 10px 16px;
 }
 @media only screen and (max-width: 539px) {
+  h2.title-home {
+    font-size: 20px;
+    line-height: 30px;
+  }
+  h3 {
+    font-size: 13px;
+  }
+  .menu-container {
+    padding: 30px;
+  }
+  img.menu-icon {
+    width: 65px;
+  }
 }
 </style>
