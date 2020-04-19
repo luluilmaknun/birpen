1e26813e104f81c4b0e856ce5b3c01b4e264374e
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-04-20 05:26:00 +0700

    [REFACTOR] fix: api url

--- src/services/alumniServices.js
+++ src/services/alumniServices.js
@@ -2,6 +2,6 @@ import api from '@/services/api';
 
 export default {
   toggleBlockAlumni(username, request) {
-    return api.patch('alumni/'+username+'/blacklist/', request);
+    return api.patch('alumni/'+username+'/block/', request);
   },
 };
