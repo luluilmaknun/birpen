a3e5ca9eb02a36911ab51cd535717b6673053ca5
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-19 09:40:37 +0700

    [GREEN] Only mahasiswa, asdos, dosen, and admin that can access pengumuman api

--- pengumuman/permissions.py
+++ pengumuman/permissions.py
@@ -9,3 +9,14 @@ class IsPrivilegedToCreateAnnouncemment(permissions.BasePermission):
         return request.user.is_admin() or \
                request.user.is_dosen() or \
                request.user.is_asdos()
+
+
+class IsPrivilegedToAccessAnnouncemment(permissions.BasePermission):
+    """
+    Permission class to create announcement
+    """
+    def has_permission(self, request, view):
+        return request.user.is_admin() or \
+               request.user.is_dosen() or \
+               request.user.is_asdos() or \
+               request.user.is_mahasiswa()
