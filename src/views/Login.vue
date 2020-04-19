691b6257c3051f326e68d21c83d395b5684fb8b8
athallahannafis <athallahannafis@gmail.com>
2020-04-20 03:25:50 +0700

    [REFACTOR] Delete long comments

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,11 +2,6 @@
   <div id="login-page" class="page-container" style="padding-top:0px!important">
     <h1>Masuk</h1>
     <div class="login-container">
-      <!--
-        v-model -> Value yang di-passing ke variabel "username" dan "password"
-        di dalam <script>, bisa dipakai untuk keperluan backend-nya. Lebih
-        jelasnya lihat console.log ketika klik "Masuk"
-       -->
       <!-- USERNAME -->
       <div class="username-class">
         <h2 class="font-id" id="username-id">Username:</h2>
