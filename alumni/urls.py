82b315f0a480990542a3cc103d7bf2c212af8b26
Ahmad Fauzan Amirul Isnain <ahmad.fauzan71@ui.ac.id>
2020-04-20 16:59:18 +0700

    #70 API Block/Unblock Alumni

--- alumni/urls.py
+++ alumni/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import alumni_placeholder_views, register
+from .views import alumni_placeholder_views, register, update_block_status
 
 
 urlpatterns = [
     path('', alumni_placeholder_views, name='alumni_placeholder_views'),
     path('register/', register, name='register'),
+    path('<username>/block/', update_block_status, name='update_block_status'),
 ]
