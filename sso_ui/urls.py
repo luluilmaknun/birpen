bf28be1773b4ab8f77b028e0029daa8ac92ce020
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-23 04:47:11 +0700

    [GREEN] Refresh data on page access

--- sso_ui/urls.py
+++ sso_ui/urls.py
@@ -2,9 +2,7 @@
 from django.urls import path
 from django_cas_ng import views as cas_views
 
-from rest_framework_jwt.views import refresh_jwt_token
-
-from .views import save_user_info, obtain_user_info
+from .views import save_user_info, obtain_user_info, refresh_data
 
 app_name = 'sso_ui'
 urlpatterns = [
@@ -12,5 +10,5 @@ urlpatterns = [
     path('logout/', cas_views.LogoutView.as_view(), name='logout'),
     path('save_user_info/', save_user_info, name='save_user_info'),
     path('obtain-user-info/', obtain_user_info, name='obtain_user_info'),
-    path('refresh-token/', refresh_jwt_token, name='refresh_token'),
+    path('refresh-data/', refresh_data, name='refresh_data'),
 ]
