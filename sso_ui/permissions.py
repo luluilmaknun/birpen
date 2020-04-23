2623fec5ce631ab7907246aea012ac6bade394f2
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-23 12:35:03 +0700

    [CHORES] Remove unused permission class

--- sso_ui/permissions.py
+++ sso_ui/permissions.py
@@ -1,29 +1,5 @@
 from rest_framework import permissions
 
-
-class isAdmin(permissions.BasePermission):
-    """
-    Permission class for admin
-    """
-    def has_permission(self, request, view):
-        return request.user.is_admin()
-
-
-class isDosen(permissions.BasePermission):
-    """
-    Permission class for dosen
-    """
-    def has_permission(self, request, view):
-        return request.user.is_dosen()
-
-
-class isAsdos(permissions.BasePermission):
-    """
-    Permission class for asdos
-    """
-    def has_permission(self, request, view):
-        return request.user.is_asdos()
-
 class IsNotBlocked(permissions.BasePermission):
     """
     Permission class for not blocked user
