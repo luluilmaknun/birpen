91b49338a8be6d7965e5dfa7eff7d1641814c99f
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-28 12:38:53 +0700

    [GREEN] Create pesanan model

--- permohonan_surat/models.py
+++ permohonan_surat/models.py
@@ -33,3 +33,14 @@ class SuratAkademik(models.Model):
         if user.is_mahasiswa():
             return self.harga_mahasiswa
         return self.harga_alumni
+
+
+class Pesanan(models.Model):
+    nama_pemesan = models.CharField(max_length=64)
+    npm_pemesan = models.CharField(max_length=10)
+    waktu_pemesanan = models.DateTimeField(auto_now_add=True)
+    status_bayar = models.ForeignKey(StatusBayar, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'pesanan'
+        verbose_name_plural = verbose_name
