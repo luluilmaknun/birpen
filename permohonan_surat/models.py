47a1d9edaae9d57b6e5fbb1ceb6ca062b00fdd24
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-28 12:36:58 +0700

    [GREEN] Create status bayar model

--- permohonan_surat/models.py
+++ permohonan_surat/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+
+class StatusBayar(models.Model):
+    nama = models.CharField(max_length=64, null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name = 'status bayar'
+        verbose_name_plural = verbose_name
