b264ef70a80190bd92b67b35dbf7b6005565e946
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-28 12:37:33 +0700

    [GREEN] Create status surat model

--- permohonan_surat/models.py
+++ permohonan_surat/models.py
@@ -7,3 +7,11 @@ class StatusBayar(models.Model):
     class Meta:
         verbose_name = 'status bayar'
         verbose_name_plural = verbose_name
+
+
+class StatusSurat(models.Model):
+    nama = models.CharField(max_length=64, null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name = 'status surat'
+        verbose_name_plural = verbose_name
