6bd3603ce145c776529bff34128e1082d7406fcd
Ahmad Fauzan Amirul Isnain <ahmad.fauzan71@ui.ac.id>
2020-04-29 02:54:53 +0700

    #91 Database Sistem Surat

--- permohonan_surat/models.py
+++ permohonan_surat/models.py
@@ -1,1 +1,61 @@
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class StatusBayar(models.Model):
+    nama = models.CharField(max_length=64, null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name = 'status bayar'
+        verbose_name_plural = verbose_name
+
+
+class StatusSurat(models.Model):
+    nama = models.CharField(max_length=64, null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name = 'status surat'
+        verbose_name_plural = verbose_name
+
+
+class SuratAkademik(models.Model):
+    jenis_dokumen = models.CharField(max_length=64, null=False, blank=False, unique=True)
+    harga_mahasiswa = models.PositiveIntegerField(null=False, blank=False, default=0)
+    harga_alumni = models.PositiveIntegerField(null=False, blank=False, default=0)
+
+    class Meta:
+        verbose_name = 'surat akademik'
+        verbose_name_plural = verbose_name
+
+    def harga(self, user):
+        if user.is_mahasiswa():
+            return self.harga_mahasiswa
+        return self.harga_alumni
+
+
+class Pesanan(models.Model):
+    nama_pemesan = models.CharField(max_length=64)
+    npm_pemesan = models.CharField(max_length=10)
+    waktu_pemesanan = models.DateTimeField(auto_now_add=True)
+    status_bayar = models.ForeignKey(StatusBayar, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'pesanan'
+        verbose_name_plural = verbose_name
+
+
+class PesananSuratAkademik(models.Model):
+    pesanan = models.ForeignKey(Pesanan, on_delete=models.CASCADE)
+    surat_akademik = models.ForeignKey(SuratAkademik, on_delete=models.CASCADE)
+    status_surat = models.ForeignKey(StatusSurat, on_delete=models.CASCADE)
+    jumlah = models.PositiveIntegerField(null=False, blank=False, default=0)
+
+    class Meta:
+        verbose_name = 'pesanan surat akademik'
+        verbose_name_plural = verbose_name
+        constraints = [
+            models.UniqueConstraint(fields=['pesanan', 'surat_akademik'],
+                                    name='Unique combination pesanan and surat_akademik')
+        ]
