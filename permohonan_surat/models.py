5867deef7fcfa82ff9580d1a2961338435ea2359
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-28 12:38:20 +0700

    [GREEN] Create surat akademik model

--- permohonan_surat/models.py
+++ permohonan_surat/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class StatusBayar(models.Model):
@@ -15,3 +18,18 @@ class StatusSurat(models.Model):
     class Meta:
         verbose_name = 'status surat'
         verbose_name_plural = verbose_name
+
+
+class SuratAkademik(models.Model):
+    jenis_dokumen = models.CharField(max_length=64, null=False, blank=False, unique=True)
+    harga_mahasiswa = models.PositiveIntegerField(null=False, blank=False, default=0)
+    harga_alumni = models.PositiveIntegerField(null=False, blank=False, default=0)
+
+    class Meta:
+        verbose_name = 'surat akademik'
+        verbose_name_plural = verbose_name
+
+    def harga(self, user):
+        if user.is_mahasiswa():
+            return self.harga_mahasiswa
+        return self.harga_alumni
