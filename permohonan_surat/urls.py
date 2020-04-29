832bcd5df892179dc31299d391d52bd1f318e750
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-04-29 14:13:12 +0700

    [GREEN] Implement create pesanan surat akademik API

--- permohonan_surat/urls.py
+++ permohonan_surat/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import permohonan_surat_placeholder_views
+from .views import permohonan_surat_placeholder_views, create_pesanan_surat_akademik
 
 
 urlpatterns = [
     path('', permohonan_surat_placeholder_views, name='permohonan_surat_placeholder_views'),
+    path('pesanan-surat-akademik/create/', create_pesanan_surat_akademik,
+         name='create_pesanan_surat_akademik'),
 ]
