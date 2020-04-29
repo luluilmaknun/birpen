e37b771030bbc20141fc003483fc1c0b54d7aee7
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-04-30 00:51:54 +0700

    [GREEN] Implement API read status bayar

--- permohonan_surat/urls.py
+++ permohonan_surat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import permohonan_surat_placeholder_views
+from .views import permohonan_surat_placeholder_views, read_status_bayar
 
 
 urlpatterns = [
     path('', permohonan_surat_placeholder_views, name='permohonan_surat_placeholder_views'),
+    path('status-bayar', read_status_bayar, name='read_status_bayar')
 ]
--- permohonan_surat/views.py
+++ permohonan_surat/views.py
@@ -1,6 +1,8 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 
+from .models import StatusBayar
 # Create your views here.
 
 @api_view(["GET"])
@@ -10,4 +12,14 @@ def permohonan_surat_placeholder_views(_):
     }
 
     return Response({"success": True, "result": result}, status=200)
-    
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated, ])
+def read_status_bayar(request):
+    response = {}
+    all_obj = StatusBayar.objects.all()
+
+    response["status_bayar"] = [obj.nama for obj in all_obj]
+
+    return Response(response)
