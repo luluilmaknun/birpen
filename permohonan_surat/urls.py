984d831c32b1cc6003dbb6763c4801da56b80fd7
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-05-03 15:16:09 +0700

    [REFACTOR] Add the forgotten trailing slash

--- permohonan_surat/urls.py
+++ permohonan_surat/urls.py
@@ -4,5 +4,5 @@ from .views import permohonan_surat_placeholder_views, read_status_bayar
 
 urlpatterns = [
     path('', permohonan_surat_placeholder_views, name='permohonan_surat_placeholder_views'),
-    path('status-bayar', read_status_bayar, name='read_status_bayar')
+    path('status-bayar/', read_status_bayar, name='read_status_bayar')
 ]
