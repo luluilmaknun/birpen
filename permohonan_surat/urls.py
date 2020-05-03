1f1dcbdc442a2e3dda6c65171332b0e06b313408
Ahmad Fauzan Amirul Isnain <ahmad.fauzan71@ui.ac.id>
2020-05-04 05:54:36 +0700

    #84 API Update Status Pembayaran Entity Pesanan

--- permohonan_surat/urls.py
+++ permohonan_surat/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import permohonan_surat_placeholder_views, create_pesanan_surat_akademik
+from .views import permohonan_surat_placeholder_views, create_pesanan_surat_akademik, \
+    update_status_bayar
 
 
 urlpatterns = [
     path('', permohonan_surat_placeholder_views, name='permohonan_surat_placeholder_views'),
     path('pesanan/create/', create_pesanan_surat_akademik,
          name='create_pesanan_surat_akademik'),
+    path('pesanan/<id_pesanan>/update-status-bayar/', update_status_bayar,
+         name='update_status_bayar'),
 ]
--- permohonan_surat/views.py
+++ permohonan_surat/views.py
@@ -11,8 +11,9 @@ from rest_framework.status import (
     HTTP_200_OK,
 )
 
-from .models import Pesanan, PesananSuratAkademik, SuratAkademik
-from .permissions import IsPrivilegedToRequestAcademicLetter
+from .models import Pesanan, PesananSuratAkademik, SuratAkademik, StatusBayar
+from .permissions import IsPrivilegedToRequestAcademicLetter, \
+    IsPrivilegedToUpdateAcademicLetterStatus
 
 
 @api_view(["GET"])
@@ -22,6 +23,32 @@ def permohonan_surat_placeholder_views(_):
     }
 
     return Response({"success": True, "result": result}, status=200)
+
+@csrf_exempt
+@api_view(["PATCH"])
+@permission_classes((IsAuthenticated, IsPrivilegedToUpdateAcademicLetterStatus))
+def update_status_bayar(request, id_pesanan):
+
+    try:
+        pesanan = Pesanan.objects.get(pk=id_pesanan)
+        status_bayar = StatusBayar.objects.get(nama=request.data.get("status_bayar"))
+
+    except Pesanan.DoesNotExist:
+        return Response({
+            'detail': 'Data pesanan tidak ditemukan.'
+        }, status=HTTP_400_BAD_REQUEST)
+
+    except StatusBayar.DoesNotExist:
+        return Response({
+            'detail': 'Data status bayar tidak ditemukan.'
+        }, status=HTTP_400_BAD_REQUEST)
+
+    pesanan.status_bayar = status_bayar
+    pesanan.save()
+
+    return Response({
+        "success": True,
+    }, status=HTTP_200_OK)
 
 @csrf_exempt
 @api_view(["POST"])
