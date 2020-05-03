d80a15dba367d45d200e71eab4db2d3ef0d31573
athallahannafis <athallahannafis@gmail.com>
2020-05-03 10:50:08 +0700

    [REFACTOR] Minor change in dummy data

--- src/views/DetailPengajuanSurat.vue
+++ src/views/DetailPengajuanSurat.vue
@@ -51,11 +51,11 @@ export default {
       listSurat: [
         {
           nama_surat: 'Transkrip nilai',
-          nama_status_surat: 'Belum Bayar',
+          nama_status_surat: 'Menunggu paraf dari Wakil Dekan',
         },
         {
           nama_surat: 'Keterangan Mahasiswa FIB UI',
-          nama_status_surat: 'Lunas',
+          nama_status_surat: 'Selesai',
         },
       ],
     };
