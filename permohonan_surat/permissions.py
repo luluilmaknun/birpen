edc7597fc8ec9294a08551ce6b183b82e4027107
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-05-04 17:14:20 +0700

    [REFACTOR] Delete function re-defined

--- permohonan_surat/permissions.py
+++ permohonan_surat/permissions.py
@@ -1,14 +1,6 @@
 from rest_framework import permissions
 
 
-class IsPrivilegedToUpdateAcademicLetterStatus(permissions.BasePermission):
-    """
-    Permission class to create announcement
-    """
-    def has_permission(self, request, view):
-        return request.user.is_admin()
-
-
 class IsPrivilegedToRequestAcademicLetter(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_mahasiswa() or \
