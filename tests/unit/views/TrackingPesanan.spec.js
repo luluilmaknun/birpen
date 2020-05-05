b775925cd0ab7eb1961aa9c5e98643cae2c1a4a2
athallahannafis <athallahannafis@gmail.com>
2020-05-05 21:27:23 +0700

    [RED] Add test for implementing get tracking pesanan

--- tests/unit/views/TrackingPesanan.spec.js
+++ tests/unit/views/TrackingPesanan.spec.js
@@ -1,8 +1,30 @@
 import {shallowMount} from '@vue/test-utils';
 import TrackingPesanan from '@/views/TrackingPesanan.vue';
+import trackingPesananApi from '@/services/trackingPesananServices.js';
 
 describe('Cek komponen tabel', () => {
-  const wrapper = shallowMount(TrackingPesanan);
+  let vm; let wrapper;
+  const trackingList = [
+    {
+      'nama_pemesan': 'Ardo',
+      'npm_pesanan': '1706075200',
+      'pk': '12',
+      'status_bayar': 'Belum Bayar',
+      'waktu_pemesanan': '3 Januari 2020',
+    },
+  ];
+  beforeEach(() => {
+    wrapper = shallowMount(TrackingPesanan);
+    vm = wrapper.vm;
+    trackingPesananApi.getTrackingPesanan = jest.fn(
+        (result) => Promise.resolve({
+          status: 200,
+          data: {
+            pesanan: trackingList,
+          },
+        }));
+    vm.fetchTrackingPesanan();
+  });
 
   it('Tes kolom pk', () => {
     expect(wrapper.find('#pk').exists()).toBe(true);
