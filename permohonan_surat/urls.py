f87516042bd58dd52533ab43d613b4806ba1c739
Yusuf Tri Ardho Mulyawan <yusuf.tri71@ui.ac.id>
2020-05-05 20:33:19 +0700

    #83 API Lihat Daftar Pengajuan Surat

--- permohonan_surat/urls.py
+++ permohonan_surat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import permohonan_surat_placeholder_views, create_pesanan_surat_akademik, \
-    read_status_bayar, update_status_surat, update_status_bayar
+    read_pesanan, read_pesanan_detail, update_status_surat, update_status_bayar, \
+    read_status_bayar
 
 
 urlpatterns = [
@@ -8,6 +9,8 @@ urlpatterns = [
     path('status-bayar/', read_status_bayar, name='read_status_bayar'),
     path('pesanan/create/', create_pesanan_surat_akademik,
          name='create_pesanan_surat_akademik'),
+    path('pesanan/', read_pesanan, name='read_pesanan'),
+    path('pesanan/<id_pesanan>/', read_pesanan_detail, name='read_pesanan_detail'),
     path(
         'pesanan/<id_pesanan>/surat-akademik/<jenis_dokumen>/update-status/',
         update_status_surat, name='update_status_surat',
