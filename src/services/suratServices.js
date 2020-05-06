b396f82719d3db08b8666c2f6fa9773600cf935e
athallahannafis <athallahannafis@gmail.com>
2020-05-06 22:14:06 +0700

    [CHORES] Merge get tracking to created service

--- src/services/suratServices.js
+++ src/services/suratServices.js
@@ -1,6 +1,9 @@
 import api from '@/services/api';
 
 export default {
+  getTrackingPesanan() {
+    return api.get('permohonan-surat/pesanan');
+  },
   fetchStatusSurat() {
     return api.get('permohonan-surat/status-surat/');
   },
--- src/services/trackingPesananServices.js
+++
@@ -1,7 +0,0 @@
-import api from '@/services/api';
-
-export default {
-  getTrackingPesanan() {
-    return api.get('permohonan-surat/pesanan');
-  },
-};
