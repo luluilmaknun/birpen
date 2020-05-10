0f7c1aa37b02f9fb570e90a39b105dcc568d1e3a
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-05-10 14:54:42 +0700

    [GREEN] Implement read program studi API

---
+++ karya_akhir/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework import permissions
+
+class IsPrivilegedToAccessKaryaAkhir(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_mahasiswa() or \
+               request.user.is_admin()
