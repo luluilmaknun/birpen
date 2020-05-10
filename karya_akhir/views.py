7d500eaec9a59cb03b6ae8dd7d2eae1f23821512
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-05-10 14:10:33 +0700

    [GREEN] Implements read surat karya akhir API

--- karya_akhir/views.py
+++ karya_akhir/views.py
@@ -1,8 +1,13 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_200_OK,
 )
+
+from .models import SuratKaryaAkhir
+from .permissions import IsPrivilegedToAccessKaryaAkhir
+from .serializers import SuratKaryaAkhirSerializer
 
 
 @api_view(["GET"])
@@ -12,3 +17,13 @@ def karya_akhir_placeholder_views(_):
     }
 
     return Response({"success": True, "result": result}, status=HTTP_200_OK)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated, IsPrivilegedToAccessKaryaAkhir])
+def read_surat_karya_akhir(_):
+    all_surat_karya_akhir = SuratKaryaAkhir.objects.all()
+
+    return Response({
+        "surat_karya_akhir": (SuratKaryaAkhirSerializer(surat_karya_akhir).data
+                              for surat_karya_akhir in all_surat_karya_akhir),
+    })
