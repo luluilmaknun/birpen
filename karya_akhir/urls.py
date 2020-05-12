7eb3e2d6915d90fce9b58403d8a7d17a836dda96
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2020-05-13 01:07:39 +0700

    [GREEN] Implement read data karya akhir by username API

--- karya_akhir/urls.py
+++ karya_akhir/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import karya_akhir_placeholder_views
+from .views import karya_akhir_placeholder_views, read_data_karya_akhir_by_username
 
 urlpatterns = [
     path('', karya_akhir_placeholder_views, name='karya_akhir_placeholder_views'),
+    path('<username>/', read_data_karya_akhir_by_username,
+         name='read_data_karya_akhir_by_username'),
 ]
--- karya_akhir/views.py
+++ karya_akhir/views.py
@@ -1,8 +1,14 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_200_OK,
+    HTTP_404_NOT_FOUND,
 )
+
+from .models import DataKaryaAkhir
+from .permissions import IsPrivilegedToReadDataKaryaAkhir
+from .serializers import DataKaryaAkhirSerializer
 
 
 @api_view(["GET"])
@@ -12,3 +18,18 @@ def karya_akhir_placeholder_views(_):
     }
 
     return Response({"success": True, "result": result}, status=HTTP_200_OK)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated, IsPrivilegedToReadDataKaryaAkhir])
+def read_data_karya_akhir_by_username(_, username):
+    try:
+        data_karya_akhir = DataKaryaAkhir.objects.get(mahasiswa__username=username)
+
+    except DataKaryaAkhir.DoesNotExist:
+        return Response({
+            "detail": "Data karya akhir tidak ditemukan.",
+        }, status=HTTP_404_NOT_FOUND)
+
+    return Response({
+        "data_karya_akhir": DataKaryaAkhirSerializer(data_karya_akhir).data
+    }, status=HTTP_200_OK)
