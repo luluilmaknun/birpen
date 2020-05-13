5d0ec8a8f6edb9fc84fa78bdc48272acbcca9482
Ahmad Fauzan Amirul Isnain <ahmad.fauzan71@ui.ac.id>
2020-05-13 12:20:17 +0700

    #117 Database Tugas Akhir

---
+++ karya_akhir/models.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class ProgramStudi(models.Model):
+    """
+    Used for listing study program, in case need of study program list
+    """
+    nama = models.CharField(max_length=32, null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name = 'program studi'
+        verbose_name_plural = verbose_name
+
+
+class SuratKaryaAkhir(models.Model):
+    nama = models.CharField(max_length=100, null=False, blank=False, unique=True)
+
+    class Meta:
+        verbose_name = 'surat karya akhir'
+        verbose_name_plural = verbose_name
+
+
+class JenisKaryaAkhir(models.Model):
+    nama = models.CharField(max_length=20, null=False, blank=False, unique=True)
+    class Meta:
+        verbose_name = 'jenis karya akhir'
+        verbose_name_plural = verbose_name
+
+
+class DataKaryaAkhir(models.Model):
+    mahasiswa = models.OneToOneField(User, on_delete=models.CASCADE)
+    peminatan_mahasiswa = models.CharField(max_length=100, null=False, blank=False)
+    jenis_karya_akhir = models.ForeignKey(JenisKaryaAkhir, on_delete=models.CASCADE)
+    sks_diperoleh = models.PositiveIntegerField(null=False, blank=False)
+    pembimbing = models.CharField(max_length=70, null=False, blank=False)
+    pembimbing_pendamping = models.CharField(max_length=70, blank=True)
+    judul_karya_id = models.TextField(null=False, blank=False)
+    judul_karya_en = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = 'data karya akhir'
+        verbose_name_plural = verbose_name
