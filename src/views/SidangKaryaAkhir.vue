e1ed91735d271100d0998d6694d1b396f66bd22d
athallahannafis <athallahannafis@gmail.com>
2020-05-16 12:01:37 +0700

    [CHORES] Add input nama angkatan and dummy dropdown

--- src/views/SidangKaryaAkhir.vue
+++ src/views/SidangKaryaAkhir.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h1>Mahasiswa Sidang Karya Akhir</h1>
-    <div class="filter-div">
-      <select aria-placeholder="Pilih Program Studi"
-      v-model="programStudi">
-        <option>Pilihan pertama</option>
-        <option>Pilihan kedua</option>
-        <option>Pilihan ketiga</option>
-      </select>
+    <div class="main-container">
+      <div class="filter-div">
+        <select class="filter-element" id="studi-choices" v-model="chosenStudi">
+          <option :value="''" hidden>Pilih Program Studi</option>
+          <option v-for="studi in programStudiDummy"
+          :key="studi.pk"
+          :value="studi.nama">
+            {{ studi.nama }}
+          </option>
+        </select>
+        <input placeholder="Tulis nama angkatan"
+        class="filter-element" id="angkatan-input" v-model="angkatan"/>
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +23,26 @@ import apiSidangAkhir from '@/services/sidangKaryaAkhirServices.js';
 export default {
   data: function() {
     return {
-      programStudi: '',
+      angkatan: '',
+      chosenStudi: '',
+      programStudiDummy: [
+        {
+          pk: 1,
+          nama: 'Manajemen'
+        },
+        {
+          pk: 2,
+          nama: 'Akuntansi'
+        },
+        {
+          pk: 3,
+          nama: 'Bisnis Islam'
+        },
+        {
+          pk: 4,
+          nama: 'Ilmu Ekonomi Islam'
+        },
+      ],
       programStudiResponse: {},
     };
   },
@@ -35,5 +60,20 @@ export default {
 </script>
 
 <style scoped>
-
+.filter-div {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+}
+.filter-element {
+  text-align: center;
+  width: 200px;
+  background: none;
+  border-style: solid;
+  border-width: 2px;
+  border-color: black;
+  background-color: white;
+  border-radius: 10px;
+  padding: 10px 10px;
+}
 </style>
