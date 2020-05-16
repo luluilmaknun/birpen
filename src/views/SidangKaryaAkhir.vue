d1270151f60e75e906661126119d342b10f24ba8
athallahannafis <athallahannafis@gmail.com>
2020-05-16 09:26:12 +0700

    [CHORES] Add Sidang karya akhir view

---
+++ src/views/SidangKaryaAkhir.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Mahasiswa Sidang Karya Akhir</h1>
+    <div class="filter-div">
+      <select aria-placeholder="Pilih Program Studi"
+      v-model="programStudi">
+        <option>Pilihan pertama</option>
+        <option>Pilihan kedua</option>
+        <option>Pilihan ketiga</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      programStudi: '',
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
