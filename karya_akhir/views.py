b8ccf4a89d6bba89911783180f6b7705111247ab
Ahmad Fauzan Amirul Isnain <ahmad.fauzan71@ui.ac.id>
2020-05-17 13:35:38 +0700

    Merge branch 'ahmad_fauzan458/117' of https://gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2020/pplukan-feb-ui-biro-pendidikan-online into ahmad_fauzan458/119

--- karya_akhir/views.py
+++ karya_akhir/views.py
@@ -1,9 +1,13 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_200_OK,
 )
 
+from .models import ProgramStudi
+from .permissions import IsPrivilegedToAccessKaryaAkhir
+from .serializers import ProgramStudiSerializer
 
 @api_view(["GET"])
 def karya_akhir_placeholder_views(_):
@@ -12,3 +16,13 @@ def karya_akhir_placeholder_views(_):
     }
 
     return Response({"success": True, "result": result}, status=HTTP_200_OK)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated, IsPrivilegedToAccessKaryaAkhir])
+def read_program_studi(_):
+    all_program_studi = ProgramStudi.objects.all()
+
+    return Response({
+        "program_studi": (ProgramStudiSerializer(program_studi).data
+                          for program_studi in all_program_studi),
+    })
