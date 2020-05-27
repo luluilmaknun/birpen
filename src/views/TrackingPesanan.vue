f41c0988d94440aecde47028d71c6b805ee88160
athallahannafis <athallahannafis@gmail.com>
2020-05-27 19:57:09 +0700

    [CHORES] Fix margin table

--- src/views/TrackingPesanan.vue
+++ src/views/TrackingPesanan.vue
@@ -152,6 +152,9 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 100px;
+  margin-top: 20px;
+
 }
 .table-div table {
   border-radius: 1em;
