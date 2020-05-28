8c80e6a975b6cca60b6da468ffd52d90372f5058
athallahannafis <athallahannafis@gmail.com>
2020-05-29 00:23:14 +0700

    [GREEN] Improve unit testing

--- tests/unit/views/TrackingPesanan.spec.js
+++ tests/unit/views/TrackingPesanan.spec.js
@@ -4,13 +4,13 @@ import trackingPesananApi from '@/services/suratServices.js';
 
 describe('Cek komponen tabel', () => {
   let vm; let wrapper;
-  const trackingList = [
+  const response = [
     {
       'nama_pemesan': 'Ardo',
       'npm_pesanan': '1706075200',
-      'pk': '12',
+      'pk': '000012',
       'status_bayar': 'Belum Bayar',
-      'waktu_pemesanan': '2020-05-06T06:39:12.00+07:00',
+      'waktu_pemesanan': '27 Mei 2020',
     },
   ];
   beforeEach(() => {
@@ -20,29 +20,79 @@ describe('Cek komponen tabel', () => {
         (result) => Promise.resolve({
           status: 200,
           data: {
-            pesanan: trackingList,
+            pesanan: response,
           },
         }));
     vm.fetchTrackingPesanan();
+    const trackingList = vm.trackingList;
+    const pagedTrackingList = vm.pagedTrackingList;
+    vm.fetchPagination(trackingList, pagedTrackingList);
+    vm.renderPagination(1, pagedTrackingList);
   });
 
-  it('Tes kolom pk', () => {
-    expect(wrapper.find('#pk').exists()).toBe(true);
+  it('test ada response', () => {
+    expect(vm.trackingList.length).toBe(1);
+  });
+});
+
+
+describe('test button pagination', () => {
+  const wrapper = shallowMount(TrackingPesanan);
+
+  it('test untuk button increamant', () => {
+    const nextButton = wrapper.find('#next-button');
+    expect(nextButton.exists()).toBe(true);
+    nextButton.trigger('click');
   });
 
-  it('Tes kolom nama pemesan', () => {
-    expect(wrapper.find('#nama_pemesan').exists()).toBe(true);
+  it('test untuk button decreament', () => {
+    const prevButton = wrapper.find('#prev-button');
+    expect(prevButton.exists()).toBe(true);
+    prevButton.trigger('click');
   });
+});
 
-  it('Tes kolom waktu pemesanan', () => {
-    expect(wrapper.find('#waktu_pemesanan').exists()).toBe(true);
-  });
+describe('test fungsi modify date', () => {
+  const wrapper = shallowMount(TrackingPesanan);
+  const vm = wrapper.vm;
+  const defaultDateTime = '2020-05-06T06:39:12.00+07:00';
+  const result = vm.modifyDateTime(defaultDateTime);
+  expect(result).toBe('2020-05-06 06:39:12');
+});
 
-  it('Tes kolom status bayar', () => {
-    expect(wrapper.find('#status_bayar').exists()).toBe(true);
-  });
+describe('test fungsi get nama bulan', () => {
+  const wrapper = shallowMount(TrackingPesanan);
+  const vm = wrapper.vm;
+  const monthNumber = 2;
+  const result = vm.getMonthName(monthNumber);
+  expect(result).toBe('Februari');
+});
 
-  it('Tes kolom aksi', () => {
-    expect(wrapper.find('#aksi').exists()).toBe(true);
+// describe('test fungsi get date', () => {
+//   const wrapper = shallowMount(TrackingPesanan);
+//   const vm = wrapper.vm;
+//   const dateTime = '2020-05-06 06:39:12';
+//   const result = vm.getDate(dateTime);
+//   expect(result).toBe('5 Mei 2020');
+// });
+
+describe('test fungsi show detail page', () => {
+  const wrapper = shallowMount(TrackingPesanan, {
+    mocks: {
+      $router: {
+        push: jest.fn(),
+      },
+    },
   });
+  const vm = wrapper.vm;
+  global.window = Object.create(window);
+  const pk = 12;
+  const url = 'https://birpen.com' + '/surat/tracking/' + pk + '/detail/';
+  Object.defineProperty(window, 'location', {
+    value: {
+      href: url,
+    },
+  });
+  vm.showDetailPage(pk);
+  expect(window.location.href).toEqual(url);
 });
