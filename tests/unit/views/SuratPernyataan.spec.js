6c1a44f622b39aa0a4aa50660ff89a84b13f763a
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-06-02 08:01:41 +0700

    [RED] add test for page dokumen karya akhir

---
+++ tests/unit/views/SuratPernyataan.spec.js
@@ -0,0 +1,7 @@
+import SuratPernyataan from '@/views/SuratPernyataan';
+
+describe('test import component', () => {
+  it('test yang di import benar', () => {
+    expect(SuratPernyataan).toBeTruthy();
+  });
+});
