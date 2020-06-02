3104024e37c9d5f0ee0389b560d4b78f1e4b72ca
Lulu Ilmaknun Qurotaini <lulu.ilmaknun.q@gmail.com>
2020-06-02 12:26:53 +0700

    [REFACTOR] Add forgotten coma in karya_akhir urls

--- karya_akhir/urls.py
+++ karya_akhir/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', karya_akhir_placeholder_views, name='karya_akhir_placeholder_views'),
     path('surat/', read_surat_karya_akhir, name='read_surat_karya_akhir'),
     path('program-studi/', read_program_studi, name='read_program_studi'),
-    path('filter-mahasiswa', filter_mahasiswa, name='filter_mahasiswa')
+    path('filter-mahasiswa', filter_mahasiswa, name='filter_mahasiswa'),
     path('<username>/', read_data_karya_akhir_by_username,
          name='read_data_karya_akhir_by_username'),
 ]
