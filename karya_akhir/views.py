0d8ed19a5e21f0dad908edf0a22bd35c08ceff93
Ahmad Fauzan Amirul Isnain <ahmad.fauzan71@ui.ac.id>
2020-06-03 04:28:03 +0700

    Merge branch 'ahmad_fauzan458/117' of https://gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2020/pplukan-feb-ui-biro-pendidikan-online into ahmad_fauzan458/112

--- karya_akhir/views.py
+++ karya_akhir/views.py
@@ -11,7 +11,7 @@ from rest_framework.status import (
 )
 
 from .permissions import IsPrivilegedToReadDataKaryaAkhir, IsPrivilegedToAccessKaryaAkhir, \
-    IsPrivilegedToCreateKaryaAkhir, IsAdmin
+    IsPrivilegedToCreateKaryaAkhir, IsAdmin, IsPrivilegedToEditDataKaryaAkhir
 from .models import DataKaryaAkhir, SuratKaryaAkhir, ProgramStudi, JenisKaryaAkhir
 from .serializers import DataKaryaAkhirSerializer, SuratKaryaAkhirSerializer, \
     ProgramStudiSerializer, MahasiswaKaryaAkhirSerializer, JenisKaryaAkhirSerializer
@@ -25,6 +25,37 @@ def karya_akhir_placeholder_views(_):
 
     return Response({"success": True, "result": result}, status=HTTP_200_OK)
 
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated, IsPrivilegedToEditDataKaryaAkhir])
+def edit_data_karya_akhir(request):
+    try:
+        data_karya_akhir = request.user.data_karya_akhir
+        data_karya_akhir.peminatan_mahasiswa = request.data.get('peminatan_mahasiswa')
+        data_karya_akhir.jenis_karya_akhir = \
+            JenisKaryaAkhir.objects.get(nama=request.data.get('jenis_karya_akhir'))
+        data_karya_akhir.sks_diperoleh = request.data.get('sks_diperoleh')
+        data_karya_akhir.pembimbing = request.data.get('pembimbing')
+        data_karya_akhir.pembimbing_pendamping = request.data.get('pembimbing_pendamping')
+        data_karya_akhir.judul_karya_id = request.data.get('judul_karya_id')
+        data_karya_akhir.judul_karya_en = request.data.get('judul_karya_en')
+        data_karya_akhir.save()
+
+    except DataKaryaAkhir.DoesNotExist:
+        return Response({
+            "success": False,
+            "detail": "Data karya akhir untuk " + request.user.username + " tidak ditemukan",
+        }, status=HTTP_400_BAD_REQUEST)
+
+    except (JenisKaryaAkhir.DoesNotExist,
+            IntegrityError, DataError, ValueError):
+        return Response({
+            "success": False,
+            "detail": "Data tidak valid",
+        }, status=HTTP_400_BAD_REQUEST)
+
+    return Response({
+        "success": True,
+    }, status=HTTP_200_OK)
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated, IsPrivilegedToCreateKaryaAkhir])
