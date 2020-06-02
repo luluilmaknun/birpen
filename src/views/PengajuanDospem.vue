8965f506fb05872586856203ed40877e8e9b334e
yusuf.tri71 <yusuf.tri71@ui.ac.id>
2020-06-02 08:02:07 +0700

    [GREEN] implement page dokumen karya akhir

---
+++ src/views/PengajuanDospem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="PengajuanDospem">
+    <div class="title-container">
+      <h1>Formulir Pengajuan Dosen<br>Pembimbing Karya Akhir</h1>
+    </div>
+    <div class="pdf-viewer">
+      <!-- TODO -->
+    </div>
+    <div class="bot-container">
+      <SaveBtn id="btn" />
+      <BackBtn id="btn" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BackBtn from '@/components/btn-kembali';
+import SaveBtn from '@/components/btn-simpan';
+
+export default {
+  name: 'PengajuanDospem',
+  components: {BackBtn, SaveBtn},
+};
+</script>
+
+<style scoped>
+.title-container {
+  text-align: center;
+  margin: 20px;
+}
+
+.bot-container {
+  display: flex;
+  flex-direction: row;
+  margin: 20px;
+}
+
+.bot-container #btn {
+  margin: 30px;
+}
+</style>
---
+++ src/views/PermohonanUjianBersyarat.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="PermohonanUjianBersyarat">
+    <div class="title-container">
+      <h1>Surat Permohonan Mengikuti Ujian Karya<br>
+        Akhir Bersyarat (UKAB)
+      </h1>
+    </div>
+    <div class="pdf-viewer">
+      <!-- TODO -->
+    </div>
+    <div class="bot-container">
+      <SaveBtn id="btn" />
+      <BackBtn id="btn" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BackBtn from '@/components/btn-kembali';
+import SaveBtn from '@/components/btn-simpan';
+
+export default {
+  name: 'PermohonanUjianBersyarat',
+  components: {BackBtn, SaveBtn},
+};
+</script>
+
+<style scoped>
+.title-container {
+  text-align: center;
+  margin: 20px;
+}
+
+.bot-container {
+  display: flex;
+  flex-direction: row;
+  margin: 20px;
+}
+
+.bot-container #btn {
+  margin: 30px;
+}
+</style>
