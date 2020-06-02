21edf1d2e326c9be7575e0ba7dcb5ef41d594cd2
Ahmad Fauzan Amirul Isnain <ahmad.fauzan71@ui.ac.id>
2020-06-02 12:17:33 +0700

    Merge branch 'ahmad_fauzan458/117' of https://gitlab.cs.ui.ac.id/ppl-fasilkom-ui/2020/pplukan-feb-ui-biro-pendidikan-online into ahmad_fauzan458/113

--- karya_akhir/permissions.py
+++ karya_akhir/permissions.py
@@ -1,5 +1,12 @@
 from rest_framework import permissions
 
+class IsPrivilegedToReadDataKaryaAkhir(permissions.BasePermission):
+    def has_permission(self, request, view):
+        username = view.kwargs.get('username', None)
+
+        return request.user.is_admin() or \
+               (request.user.is_mahasiswa() and request.user.username == username)
+
 class IsPrivilegedToAccessKaryaAkhir(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_mahasiswa() or \
