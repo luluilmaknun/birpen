b7eab7addb13df81d64988bfc0063a37810dac6a
ANNIDA SAFIRA ARIEF <annida.safira@ui.ac.id>
2020-06-03 14:18:44 +0700

    [CHORES] Reorder status bayar seed
    Put initial state as first data

--- asdos/admin.py
+++ asdos/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 
 app = apps.get_app_config('asdos')
 
+class DisplayUsername(admin.ModelAdmin):
+    list_display = ['username']
+
 for model_name, model in app.models.items():
-    admin.site.register(model)
+    admin.site.register(model, DisplayUsername)
