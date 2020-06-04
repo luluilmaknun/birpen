bd353db6c7eb5a62f26716bfbcca7bd915513db9
Athallah Annafis <athallah.annafis@ui.ac.id>
2020-06-04 12:31:38 +0700

    Pagination table

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -21,7 +21,7 @@
           </th>
         </tr>
 
-        <tr v-for="admin in listAdmin" :key="admin.username">
+        <tr v-for="admin in renderPagedList" :key="admin.username">
           <td id="username">
             {{ admin.username }}
           </td>
@@ -30,6 +30,36 @@
           </td>
         </tr>
       </table>
+    </div>
+    <!-- PAGINATION -->
+    <div class="pagination-section">
+      <div class="button-box-2">
+        <button class="pagination-button"
+        id="first-page-button" v-show="showFirst" v-on:click="this.toFirstPage">
+          &lt;&lt;
+        </button>
+      </div>
+      <div class="button-box">
+        <button class="pagination-button"
+        id="prev-button" v-show="showPrev" v-on:click="this.decreamentPage">
+          &lt;
+        </button>
+      </div>
+      <div class="button-box">
+        <p class="page-number">{{ pageNumber }} of {{ pagedList.length }}</p>
+      </div>
+      <div class="button-box">
+        <button class="pagination-button"
+        id="next-button" v-show="showNext" v-on:click="this.increamentPage">
+          &gt;
+        </button>
+      </div>
+      <div class="button-box-2">
+        <button class="pagination-button"
+        id="last-page-button" v-show="showLast" v-on:click="this.toLastPage">
+          &gt;&gt;
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -53,6 +83,13 @@ export default {
       response: {},
       listAdmin: [],
       isFetchAdmin: false,
+      showPrev: true,
+      showNext: true,
+      showFirst: true,
+      showLast: true,
+      pagedList: [],
+      renderPagedList: [],
+      pageNumber: 1,
     };
   },
   created() {
@@ -66,8 +103,80 @@ export default {
         for (let i = 0; i < this.response.admin.length; i++) {
           this.$set(this.listAdmin, i, this.response.admin[i]);
         }
+        this.fetchPagination(this.listAdmin, this.pagedList);
+        this.renderPagination(this.pageNumber, this.pagedList);
         this.isFetchAdmin = false;
       });
+    },
+    fetchPagination: function(theList, pagedList) {
+      const base = 5;
+      let temp = [];
+      let count = 0;
+      for (let i = 0; i < theList.length; i++) {
+        temp.push(theList[i]);
+        count += 1;
+        if (count == base) {
+          count = 0;
+          pagedList.push(temp);
+          temp = [];
+        } if (i == theList.length-1) {
+          pagedList.push(temp);
+          temp = [];
+        }
+      }
+      if (pagedList[pagedList.length - 1] == 0) {
+        const last = pagedList.length - 1;
+        pagedList.splice(last, 1);
+      }
+    },
+    renderPagination: function(pageNumber, pagedList) {
+      this.checkPaginationButton(pageNumber, pagedList);
+      const tempList = pagedList[pageNumber-1];
+      this.renderPagedList = tempList;
+    },
+    increamentPage: function() {
+      this.pageNumber++;
+      this.renderPagination(this.pageNumber, this.pagedList);
+    },
+    decreamentPage: function() {
+      this.pageNumber--;
+      this.renderPagination(this.pageNumber, this.pagedList);
+    },
+    toFirstPage: function() {
+      this.pageNumber = 1;
+      this.renderPagination(this.pageNumber, this.pagedList);
+    },
+    toLastPage: function() {
+      const last = this.pagedList.length;
+      this.pageNumber = last;
+      this.renderPagination(this.pageNumber, this.pagedList);
+    },
+    checkPaginationButton: function(pageNumber, pagedList) {
+      if (pageNumber == 1 &&
+      (pagedList.length == 1 || pagedList.length == 0)) {
+        this.showPrev = false;
+        this.showNext = false;
+        this.showFirst = false;
+        this.showLast = false;
+      } else if (pageNumber == 1 && pagedList.length > 1) {
+        // ujung kiri
+        this.showPrev = false;
+        this.showNext = true;
+        this.showFirst = false;
+        this.showLast = true;
+      } else if (pageNumber == pagedList.length) {
+        // ujung kanan
+        this.showNext = false;
+        this.showPrev = true;
+        this.showFirst = true;
+        this.showLast = false;
+      } else {
+        // tengah
+        this.showNext = true;
+        this.showPrev = true;
+        this.showFirst = true;
+        this.showLast = true;
+      }
     },
   },
 };
@@ -119,5 +228,55 @@ td#username {
   .table-div {
     width: 75%;
   }
+}
+/* PAGINATION */
+.pagination-section {
+  margin-top: 30px;
+  margin-bottom: 70px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.pagination-button {
+  padding: 5px 10px;
+  border-radius: 1000px;
+  border-style: none;
+  background: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  font-weight: bolder;
+  font-size: 16pt;
+  margin: 0;
+}
+.pagination-button:hover {
+  background-color: #FFDD00;
+}
+#next-button {
+  visibility: visible;
+}
+#prev-button {
+  visibility: visible;
+}
+.button-box {
+  width: 80px;
+  height: 40px;
+}
+.button-box-2 {
+  width: 75px;
+  height: 40px;
+}
+.button-box, .button-box-2 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.page-number {
+  margin-left: 10px;
+  margin-right: 10px;
 }
 </style>
